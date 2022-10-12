abb2c039d6948a1d763091b6ce63d1e9ae27d73d
Ivan Pachev <mr.pachev@abv.bg>
2022-10-12 08:35:21 +0300

    DataTypeFinder_01

---
+++ JavaFundamentals/MoreExer_02/FromLeftToTheRight_02.java
@@ -0,0 +1,14 @@
+package MoreExer_02;
+
+import java.util.Scanner;
+
+public class FromLeftToTheRight_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
+    }
+}
