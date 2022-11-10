22a0c944eb19d0d9835c907d26374b9b0814e37b
Ivan Pachev <mr.pachev@abv.bg>
2022-11-10 14:15:36 +0200

    Update TheLift_02.java

--- JavaFundamentals/MidExamPreparation/TheLift_02.java
+++ JavaFundamentals/MidExamPreparation/TheLift_02.java
@@ -12,28 +12,54 @@ public class TheLift_02 {
 
         int peopleStay = Integer.parseInt(scanner.nextLine()); //чакащи хора за лифта
         int[] liftArray = Arrays.stream(scanner.nextLine() //лифта и хората в него
-                                .split(" "))
-                                .mapToInt(Integer::parseInt)
-                                 .toArray();
+                        .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int countFullVagons = 0; //брояч пълни вагони
 
         for (int indexArr = 0; indexArr <= liftArray.length - 1; indexArr++) { //обхождаме всеки вагон
             int neededPeople = 0; //нужни хора за запълванена дадения вагон
 
-            if (liftArray[indexArr] < 4){ //проверка дали вагона е запълнен
+            if (liftArray[indexArr] < 4) { //проверка дали вагона е запълнен
                 neededPeople = 4 - liftArray[indexArr]; //намиране колко хора са нужни за запълване на вагона
 
-                if (peopleStay > 0){ //проверка дали има хора да се качват
+                if (peopleStay > 0) { //проверка дали има хора да се качват
 
                     if (peopleStay >= neededPeople) { //запълване на вагона
                         liftArray[indexArr] = 4;
                         peopleStay -= neededPeople;
-                    }else {
+                        countFullVagons++;
+                    } else {
                         liftArray[indexArr] += peopleStay;
                         peopleStay -= peopleStay;
                     }
                 }
+            } else {
+                countFullVagons++;
             }
-
         }
+
+        if (countFullVagons == liftArray.length) { //проверка дали лифта е пълен
+
+            if (peopleStay <= 0) { //проверка дали има чакащи хора
+                System.out.println(liftArr(liftArray));
+            } else {
+                System.out.printf("There isn't enough space! %d people in a queue!%n", peopleStay);
+                System.out.println(liftArr(liftArray));
+            }
+        } else {
+            System.out.println("The lift has empty spots!");
+            System.out.println(liftArr(liftArray));
+        }
+
+    }
+
+    public static String liftArr(int[] lift) {
+        String result = "";
+        for (int vagon : lift) {
+            result += vagon + " ";
+        }
+        return result;
     }
 }
