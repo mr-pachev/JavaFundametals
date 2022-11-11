8b77fc655563365ebfa636a6e84aa28eb4ba1d1e
Ivan Pachev <mr.pachev@abv.bg>
2022-11-11 21:59:34 +0200

    Create CounterStrike_01.java

---
+++ JavaFundamentals/MidExamPreparation/CounterStrike_01.java
@@ -0,0 +1,23 @@
+package MidExamPreparation;
+
+import java.util.Scanner;
+
+public class CounterStrike_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startEnergy = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        int counterWinGames = 0; //брояч спечелени игри
+
+        while (!input.equals("End of battle")){
+            int currentEnergy = Integer.parseInt(input); //текущомразстояние и енергиа за него
+
+
+
+            input = scanner.nextLine();
+        }
+
+    }
+}
