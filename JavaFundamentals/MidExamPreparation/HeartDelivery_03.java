4257ca8004c1430dadd6f0a42d00ed90074d1864
Ivan Pachev <mr.pachev@abv.bg>
2022-11-16 20:59:22 +0200

    Update HeartDelivery_03.java

--- JavaFundamentals/MidExamPreparation/HeartDelivery_03.java
+++ JavaFundamentals/MidExamPreparation/HeartDelivery_03.java
@@ -11,68 +11,54 @@ public class HeartDelivery_03 {
                         .split("@"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
         String input = scanner.nextLine();
-        boolean isEnough = false;
-        int currentPosition = 0;
-        boolean isPass = false;
 
-        for (int indexArr = 0; indexArr <= neighborhoodArr.length - 1; indexArr++) {
+        int currentPosition = 0; //тещуща позиция със скока
 
-            if (!input.equals("Love!")) {
-                String[] inputArr = input.split(" ");
-                int jumpPower = Integer.parseInt(inputArr[1]); //сила на скока
-                currentPosition = indexArr + jumpPower;
+        while (!input.equals("Love!")) {
+            String[] inputArr = input.split(" ");
+            int jumpPower = Integer.parseInt(inputArr[1]); //сила на скока
 
-                if (isOutside(indexArr, jumpPower, neighborhoodArr)) { //проверка дали скока ще излезе от масива
-                    currentPosition = 0;
-                    indexArr = - 1;
-                    isPass = false;
-                }
-                int currentElement = neighborhoodArr[currentPosition]; //текущия елемент
+            currentPosition += jumpPower;
+            if (isOutside(currentPosition, neighborhoodArr)) { //проверка дали скока ще излезе от масива
+                currentPosition = 0;
+            }
 
-                if (currentElement >= 2) { //проверка дали къщата ще се пропусне
-                    currentElement -= 2;
-                    isPass = false;
-                    neighborhoodArr[currentPosition] = currentElement;
-                } else if (!isPass){
-                    isPass = true;
-                    System.out.printf("Place %d already had Valentine's day.%n", currentPosition);
-                }
+            int currentElement = neighborhoodArr[currentPosition]; //текущия елемент
 
-                if (currentElement == 0 && !isPass){ //проверка дали посетената къща няма нужда от сърца вече
+            if (currentElement == 0) { //проверка дали посетената къща вече е имало Св. Валентин
+                System.out.printf("Place %d already had Valentine's day.%n", currentPosition);
+            } else { //проверка дали къщата ще се пропусне
+                currentElement -= 2;
+                neighborhoodArr[currentPosition] = currentElement;
+
+                if (currentElement == 0) { //проверка дали в дадената къща е станало Св. Валентин
                     System.out.printf("Place %d has Valentine's day.%n", currentPosition);
                 }
-                input = scanner.nextLine();
-            }else {
-                System.out.printf("Cupid's last position was %d.%n", currentPosition);
-                if (isSuccessful(neighborhoodArr)){
-                    System.out.println("Mission was successful.");
-                }else {
-                    System.out.printf("Cupid has failed %d places.%n", notSuccessfulPlaces(neighborhoodArr));
-                }
-                isEnough = true;
-                break;
             }
-            if (isEnough){
-                break;
-            }
+            input = scanner.nextLine();
         }
 
+        System.out.printf("Cupid's last position was %d.%n", currentPosition);
+        if (isSuccessful(neighborhoodArr)) {
+            System.out.println("Mission was successful.");
+        } else {
+            System.out.printf("Cupid has failed %d places.%n", notSuccessfulPlaces(neighborhoodArr));
+        }
     }
 
     //проверка дали скока надхвърля масива
-    public static boolean isOutside(int currentIndex, int jump, int[] currentArr) {
-        return (currentIndex + jump > currentArr.length - 1);
+    public static boolean isOutside(int currentIndex, int[] currentArr) {
+        return (currentIndex > currentArr.length - 1);
     }
 
     //проверка дали мавива съдържа само нулеви елементи
-    public static boolean isSuccessful (int[] currentArr){
+    public static boolean isSuccessful(int[] currentArr) {
         boolean isTrue = true;
         int element = 0;
         for (int i = 0; i <= currentArr.length - 1; i++) {
             element = currentArr[i];
-            if (element != 0){
+            if (element != 0) {
                 isTrue = false;
                 break;
             }
@@ -81,12 +67,12 @@ public class HeartDelivery_03 {
     }
 
     //боряч на елементите различни от 0
-    public static Integer notSuccessfulPlaces (int[] currentArr){
+    public static Integer notSuccessfulPlaces(int[] currentArr) {
         int element = 0;
         int counterPlaces = 0;
         for (int i = 0; i <= currentArr.length - 1; i++) {
             element = currentArr[i];
-            if (element != 0){
+            if (element != 0) {
                 counterPlaces++;
             }
         }
