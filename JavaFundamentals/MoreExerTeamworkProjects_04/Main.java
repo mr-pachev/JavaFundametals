fcb1380f66ec8bc4bb4a78c7caec2fba7a35f509
Ivan Pachev <mr.pachev@abv.bg>
2022-12-14 11:12:23 +0200

    MoreExerTeamworkProjects_04

--- JavaFundamentals/MoreExerTeamworkProjects_04/Main.java
+++ JavaFundamentals/MoreExerTeamworkProjects_04/Main.java
@@ -9,33 +9,76 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine()); //брой отбори, които трябва да се ригистрират
-        String input = "";
-        List<Team> teamList = new ArrayList<>();
+
+        List<Team> teamList = new ArrayList<>(); //списък с обектите от клас екип
+        List<String> userTeamList = new ArrayList<>();
+        boolean isTrue = true;
 
         for (int currentTeam = 1; currentTeam <= n; currentTeam++) {
-            input = scanner.nextLine();
-            List<String> teamUserList = new ArrayList<>();
+            String input = scanner.nextLine();
 
+            isTrue = true;
             String creator = input.split("-")[0]; //създател на екипа
             String teamName = input.split("-")[1]; //създадения екип
 
-            Team team = new Team(creator, teamUserList); //създаване на обект
-            teamList.add(team); //пълнене на лист с обекти
+            if(teamList.isEmpty()){ //първоначално запълване на листа с екипите
+                userTeamList = new ArrayList<>();
+                userTeamList.add(creator);
+                Team team = new Team(teamName, creator, userTeamList);
+                teamList.add(team);
+                System.out.printf("Team %s has been created by %s!%n", teamName, creator); //при вече създаден екип
+                continue;
+            }
 
-            System.out.printf("Team %s has been created by %s!%n", teamName, creator);
+            for (Team team : teamList){
+               if (team.getName().equals(teamName)){ //проверка дали екипа вече е създаден
+                   System.out.printf("Team %s was already created!%n", teamName); //при вече създаден екип
+                   isTrue = false;
+                   break;
+                }else if (team.getCreator().equals(creator)){ //проверка дали създателя вече не е създавал екип
+                   System.out.printf("%s cannot create another team!", creator); //при вече създаден друг екип
+                   isTrue = false;
+                   break;
+               }
+            }
+            if (isTrue){ //проверка дали горните условия не са изпълнени
+                userTeamList = new ArrayList<>();
+                userTeamList.add(creator);
+                Team team = new Team(teamName, creator, userTeamList);
+                teamList.add(team);
+                System.out.printf("Team %s has been created by %s!%n", teamName, creator); //при създаване на екип
+            }else {
+                continue;
+            }
         }
 
         String command = scanner.nextLine();
 
-        while (!command.equals("end of assignment")){
-            String inputData = scanner.nextLine();
-
+        while (!command.equals("end of assignment")) {
+            isTrue = true;
             String user = command.split("->")[0]; //потребител, който иска да се присъедини към екипа
+            String wishTeamName = command.split("->")[1]; //желан екип за присъединяване
 
+            for (Team team : teamList){
+                if (team.getName().equals(wishTeamName)) { //проверка дали желания екип съществува
+                    isTrue = false;
+                    team.getUserList().add(user);
+                    break;
+                }
+            }
+            if (isTrue){
+                System.out.printf("Team %s does not exist!%n", wishTeamName); //при несъщестуващ екип
+            }
 
-
+            for (Team team : teamList){
+                if (team.getUserList().contains(user)){ //проверка дали потребителя вече не съществува в някой екип
+                    System.out.printf("Member %s cannot join team %s!%n", user, wishTeamName); //при вече съществуващ потребител в някой екип
+                    break;
+                }
+            }
 
             command = scanner.nextLine();
         }
+        System.out.printf("");
     }
 }
--- JavaFundamentals/MoreExerTeamworkProjects_04/Team.java
+++ JavaFundamentals/MoreExerTeamworkProjects_04/Team.java
@@ -3,17 +3,27 @@ package MoreExerTeamworkProjects_04;
 import java.util.List;
 
 public class Team {
+    private String name;
     private String creator;
     private List<String> userList;
 
-    public Team(String creator, List<String> userList){
+    public Team(String name, String creator, List<String> userList){
+        this.name = name;
         this.creator = creator;
         this.userList = userList;
 
     }
 
-    public String getCreator(){
-        return this.creator;
+   public String getName() {
+        return this.name;
+   }
+
+   public String getCreator() {
+        return  this.creator;
+   }
+
+    public List<String> getUserList() {
+        return this.userList;
     }
 
 }
