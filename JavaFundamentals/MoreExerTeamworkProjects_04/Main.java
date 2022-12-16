f752e85db6c3b221b3d002b1558a218c94a8f803
Ivan Pachev <mr.pachev@abv.bg>
2022-12-16 22:10:20 +0200

    MoreExerTeamworkProjects_04

--- JavaFundamentals/MoreExerTeamworkProjects_04/Main.java
+++ JavaFundamentals/MoreExerTeamworkProjects_04/Main.java
@@ -1,9 +1,6 @@
 package MoreExerTeamworkProjects_04;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,15 +30,16 @@ public class Main {
             }
 
             for (Team team : teamList) {
-                if (team.getName().equals(teamName)) { //проверка дали създателя вече не е създавал екип
-                    System.out.printf("Team %s was already created!%n", teamName); //при вече създаден екип
-                    isExistsTeam = true;
-                }
                 if (team.getCreator().equals(creator)) { //проверка дали потребителя иска да стане създател на още един екип
                     System.out.printf("%s cannot create another team!%n", creator); //вече създател на един екип
                     isExistsTeam = true;
                 }
+                if (team.getName().equals(teamName)) { //проверка дали създателя вече не е създавал екип
+                    System.out.printf("Team %s was already created!%n", teamName); //при вече създаден екип
+                    isExistsTeam = true;
+                }
             }
+
             if (!isExistsTeam) { //проверка дали горните условия не са изпълнени
                 List<String> userTeamList = new ArrayList<>();
                 userTeamList.add(creator); //добавяне на създателя към дадения екип
@@ -57,9 +55,9 @@ public class Main {
         boolean isBreak = false;
 
         while (!command.equals("end of assignment")) {
+
             String user = command.split("->")[0]; //потребител, който иска да се присъедини към екипа
             String wishTeamName = command.split("->")[1]; //желан екип за присъединяване
-
             int counter = 0;
             for (Team team : teamList) {
                 counter++;
@@ -74,18 +72,16 @@ public class Main {
                     }
                 }
             }
+
             if (isBreak) {
-                command = scanner.nextLine();
                 isExistsTeam = false;
                 isBreak = false;
-                continue;
-            }
-
-            for (Team team : teamList) {
-                if (team.getUserList().contains(user)) { //проверка дали потребителя вече съществува в някой екип
-                    System.out.printf("Member %s cannot join team %s!%n", user, wishTeamName); //при вече съществуващ потребител в някой екип
-                    isBreak = true;
-                    break;
+            } else {
+                for (Team team : teamList) {
+                    if (team.getUserList().contains(user)) { //проверка дали потребителя вече съществува в някой екип
+                        System.out.printf("Member %s cannot join team %s!%n", user, wishTeamName); //при вече съществуващ потребител в някой екип
+                        isBreak = true;
+                    }
                 }
             }
 
@@ -117,9 +113,9 @@ public class Main {
         List<Team> teamListSortedByCountMembers = new ArrayList<>();
         int maxNumMembers = 0;
         int currentNumMembers = 0;
-        for (Team team : finalTeamList){ //сортиране на листа с обектите отговалящи на изискванията по броя членове в низходящ ред
+        for (Team team : finalTeamList) { //сортиране на листа с обектите отговалящи на изискванията по броя членове в низходящ ред
             currentNumMembers = team.getUserList().size();
-            if (currentNumMembers >= maxNumMembers){ //проверка дали текущия лист с членове е най-големия до момента
+            if (currentNumMembers >= maxNumMembers) { //проверка дали текущия лист с членове е най-големия до момента
                 maxNumMembers = currentNumMembers;
                 team = new Team(team.getName(), team.getCreator(), team.getUserList());
                 teamListSortedByCountMembers.add(0, team);
@@ -127,16 +123,16 @@ public class Main {
         }
 
         List<Team> teamListSortedByName = new ArrayList<>();
-        for (Team team : teamListSortedByCountMembers){ //сортиране на листа с обекти отговарящи на изискванията по име на екипите по възходящ ред
+        for (Team team : teamListSortedByCountMembers) { //сортиране на листа с обекти отговарящи на изискванията по име на екипите по възходящ ред
             currentNumMembers = team.getName().toCharArray()[0];
-            if (currentNumMembers >= maxNumMembers){
+            if (currentNumMembers >= maxNumMembers) {
                 maxNumMembers = currentNumMembers;
                 team = new Team(team.getName(), team.getCreator(), team.getUserList());
                 teamListSortedByName.add(team);
             }
         }
 
-        for (Team team : teamListSortedByName){ //сортиране на членовете на всеки екип по име-възходящ ред
+        for (Team team : teamListSortedByName) { //сортиране на членовете на всеки екип по име-възходящ ред
             Collections.sort(team.getUserList());
         }
 
