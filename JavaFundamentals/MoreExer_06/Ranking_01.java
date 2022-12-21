1d3d492dcebcbe1f739df7802b3d5141bb6f65b5
Ivan Pachev <mr.pachev@abv.bg>
2022-12-21 15:36:44 +0200

    Update Ranking_01.java

--- JavaFundamentals/MoreExer_06/Ranking_01.java
+++ JavaFundamentals/MoreExer_06/Ranking_01.java
@@ -1,8 +1,6 @@
 package MoreExer_06;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Ranking_01 {
     public static void main(String[] args) {
@@ -22,7 +20,32 @@ public class Ranking_01 {
 
         inputData = scanner.nextLine();
 
-        while(!inputData.equals("end of submissions"){
+        Map<String, List<String>> coursesUsersMap = new LinkedHashMap<>(); //дневник съдържащ валидните курсове с потребителите записани в тях
+        Map<String, Integer> usersPointsMap = new LinkedHashMap<>(); //дневник с потребители и техните точки
+
+
+        while (!inputData.equals("end of submissions")) {
+
+            String currentCourse = inputData.split("=>")[0]; //текущ курс за обработка
+            String currentCoursePass = inputData.split("=>")[1]; //текущ курс за обработка
+            String currentUser = inputData.split("=>")[2]; //текущ потребител за обработка
+            int currentUserPoints = Integer.parseInt(inputData.split("=>")[3]); //точки на текущия потребител
+
+            if (coursePassMap.containsKey(currentCourse) && (coursePassMap.containsValue(currentCoursePass))){ //проверка дали текущия курс съществува в дневника на съществуващите курсове
+                List<String> usersInCourseList = coursesUsersMap.get(currentCourse);
+                int userPoint = usersPointsMap.get(currentUser);
+
+                if (usersInCourseList.contains(currentUser)){ //проверка дали дадения потребител вече не е записан в курса
+                    usersPointsMap.put(currentUser, usersPointsMap.get(currentUser) + userPoint);
+
+                }else {
+                    coursesUsersMap.put(currentCourse, new ArrayList<>());
+                    coursesUsersMap.get(currentCourse).add(currentUser);
+                }
+             
+            }
+
+            inputData = scanner.nextLine();
         }
 
     }
