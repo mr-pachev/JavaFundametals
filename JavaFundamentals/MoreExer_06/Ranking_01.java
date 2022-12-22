83f18f81ed6c99b541381d542a3353932e443e00
Ivan Pachev <mr.pachev@abv.bg>
2022-12-22 20:26:45 +0200

    Update Ranking_01.java

--- JavaFundamentals/MoreExer_06/Ranking_01.java
+++ JavaFundamentals/MoreExer_06/Ranking_01.java
@@ -12,53 +12,77 @@ public class Ranking_01 {
         while (!inputData.equals("end of contests")) {
             String course = inputData.split(":")[0]; //името на курса
             String coursePassword = inputData.split(":")[1]; //паролата на курса
-
             coursePassMap.put(course, coursePassword); //пълнена на дневника
-
             inputData = scanner.nextLine();
         }
 
         inputData = scanner.nextLine();
 
-        Map<String, List<User>> usersCoursesMap = new LinkedHashMap<>(); //дневник съдържащ потребителите и курсовете, в които участват
+        Map<String, List<User>> usersCoursesMap = new TreeMap<>(); //дневник съдържащ потребителите и курсовете, в които участват
+        List<User> userCurrentList = new ArrayList<>();
 
         while (!inputData.equals("end of submissions")) {
-
             String currentCourse = inputData.split("=>")[0]; //текущ курс за обработка
-            String currentCoursePass = inputData.split("=>")[1]; //текущ курс за обработка
+            String currentCoursePass = inputData.split("=>")[1]; //текуща парола
             String currentUser = inputData.split("=>")[2]; //текущ потребител за обработка
             int currentUserPoints = Integer.parseInt(inputData.split("=>")[3]); //точки на текущия потребител
-            User user = new User(currentCourse, currentUserPoints);
-            List<User> userCurrentList = new ArrayList<>();
 
-            if (coursePassMap.containsKey(currentCourse) && (coursePassMap.containsValue(currentCoursePass))){ //проверка дали текущия курс съществува в дневника на съществуващите курсове
+            User user = new User(currentCourse, currentUserPoints); //създаване на обект с новите данни
+            boolean isExistUser = false;
+            boolean isExistCourse = false;
 
-                if (!usersCoursesMap.containsKey(currentUser)){
+            if (coursePassMap.containsKey(currentCourse)
+                    && (coursePassMap.containsValue(currentCoursePass))) { //проверка дали текущия курс съществува в дневника на съществуващите курсове
 
+                if (usersCoursesMap.isEmpty()) {
                     userCurrentList.add(user);
-
                     usersCoursesMap.put(currentUser, userCurrentList);
-                }else {
-                   if (usersCoursesMap.containsValue(currentCourse)){
-                       
-                   }
+                    inputData = scanner.nextLine();
+                    continue;
                 }
-                System.out.println();
 
+                for (Map.Entry<String, List<User>> entry : usersCoursesMap.entrySet()) {
+                    if (entry.getKey().equals(currentUser)) { //проверка дали конкретния потребител съществува в дневника
 
+                        for (User user1 : usersCoursesMap.get(currentUser)) { //обхождаме листа с обектите
+                            if (entry.getKey().equals(currentUser) && user1.getCourses().equals(currentCourse)) { //проверяваме конкретния курс дали съществува в обекта
+                                if (user1.getPoints() <= currentUserPoints) { //проверяваме дали точките от конкретния обект са по-малко от текущите точки
+                                    user1.setPoints(currentUserPoints); //добавяне на текущите точки към обекта
+                                    isExistCourse = true;
+                                    break;
+                                }
+                            }
+                        }
+                        isExistUser = true;
+                    }
+                }
+                if (!isExistUser) { //добавяну на новия потребител с курса и точките
+                    userCurrentList = new ArrayList<>();
+                    userCurrentList.add(user);
+                    usersCoursesMap.put(currentUser, userCurrentList);
+                    inputData = scanner.nextLine();
+                    continue;
+                }
+
+                if(!isExistCourse){
+                    userCurrentList.add(user);
+                    usersCoursesMap.put(currentUser, userCurrentList); //добавяне новия курс към потребителя
+                }
             }
+
 
             inputData = scanner.nextLine();
         }
 
     }
+
     public static class User {
         String course;
         int points;
 
-        private User (String course, int points){
+        private User(String course, int points) {
             this.course = course;
-            this.points =points;
+            this.points = points;
         }
 
         public String getCourses() {
@@ -67,6 +91,10 @@ public class Ranking_01 {
 
         public int getPoints() {
             return this.points;
+        }
+
+        public void setPoints(int points) {
+            this.points = points;
         }
     }
 }
