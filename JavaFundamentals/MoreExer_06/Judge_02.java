8a899398b86f77a330911e8e5f5d9561b2b37b8f
Ivan Pachev <mr.pachev@abv.bg>
2022-12-25 17:38:14 +0200

    Update Judge_02.java

--- JavaFundamentals/MoreExer_06/Judge_02.java
+++ JavaFundamentals/MoreExer_06/Judge_02.java
@@ -11,30 +11,52 @@ public class Judge_02 {
         String inputData = scanner.nextLine();
         Map<String, Integer> userPointsMap = new LinkedHashMap<>(); //дневник с потребителя и точките за конкретния курс
         Map<String, Map<String, Integer>> coursesMap = new LinkedHashMap<>(); //дневник с курсовете, потребителите в тях и точките им
-        
-        while (!inputData.equals("no more time")){
+
+        while (!inputData.equals("no more time")) {
             String user = inputData.split(" -> ")[0]; //участник
             String course = inputData.split(" -> ")[1]; //конкуркс
             int points = Integer.parseInt(inputData.split(" -> ")[2]); //точки
+            boolean isExistUser = false;
 
             if (!coursesMap.containsKey(course)) { //проверка дали курса не съществува
                 userPointsMap = new LinkedHashMap<>();
                 userPointsMap.put(user, points);
                 coursesMap.put(course, userPointsMap);
-            }else {
+            } else {
                 //обхождане на целиs дневник
-               for(Map.Entry<String, Map<String, Integer>> entry : coursesMap.entrySet()){
-                   int maxPoints = 0;
-                   for (Map.Entry<String, Integer> entry1 : userPointsMap.entrySet()){
-                       if (!entry1.getKey().equals(user)){ //проверка дали потребителя не съществува за съответния курс
-                           //добавяне на нов потребител към съществуващия курс
-                           userPointsMap.put(user, points);
-                           coursesMap.put(course, userPointsMap);
-                       }else {
+                int maxPoints = 0;
+                for (Map.Entry<String, Map<String, Integer>> entry : coursesMap.entrySet()) {
 
-                       }
-                   }
-               }
+                    if (entry.getKey().equals(course)) { //осигурява обхождане само на конкретния курс
+                        for (Map.Entry<String, Integer> entry1 : entry.getValue().entrySet()) {
+                            userPointsMap = entry.getValue();
+
+                            if (entry1.getKey().equals(user)) { //проверка дали потребителя съществува за съответния курс
+
+                                if (points >= maxPoints) {
+                                    maxPoints = points;
+                                } else {
+                                    maxPoints = entry1.getValue();
+                                }
+                                isExistUser = true;
+                                break;
+                            }
+                        }
+                        if (!isExistUser) {
+                            maxPoints = points;
+                        }
+                    }
+
+//                    if (!isExistUser) {
+//                        //добавяне на нов потребител към съществуващия курс
+//                        userPointsMap.put(user, maxPoints);
+//                        coursesMap.put(course, userPointsMap);
+//                    }
+
+                }
+                //добавяне на нов потребител към съществуващия курс
+                userPointsMap.put(user, maxPoints);
+                coursesMap.put(course, userPointsMap);
 
             }
 
