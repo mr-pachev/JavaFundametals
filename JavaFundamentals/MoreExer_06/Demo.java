a9757fc489b20c0369c0fcd834f7a69c03c6115e
Ivan Pachev <mr.pachev@abv.bg>
2023-01-04 21:13:09 +0200

    Create Demo.java

---
+++ JavaFundamentals/MoreExer_06/Demo.java
@@ -0,0 +1,68 @@
+package MoreExer_06;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<String, User> usersMap = new LinkedHashMap<>();
+
+
+        while (!input.equals("no more time")) {
+            String[] inputData = input.split("\\s+->\\s+");
+
+            String name = inputData[0];
+            String contest = inputData[1];
+            int points = Integer.parseInt(inputData[2]);
+
+            if (!usersMap.containsKey(name)) { //проверка дали участника не съществува
+                usersMap.put(name, new User(contest, points));
+            } else {
+                if (!usersMap.get(name).getConstestName().equals(contest)) { //проверка дали курса на чустаника не съществува
+                    usersMap.put(name, new User(contest, points));
+                }else {
+                    usersMap.entrySet().stream()
+                            .map()
+
+                    if (maxPoints <= points){ //проверка дали новите точки са повече
+                        usersMap.get(name).setPoints(points);
+                    }
+                }
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println();
+    }
+
+    static class User {
+        private String constestName;
+        private int points;
+
+        public User(String constestName, int points) {
+            this.constestName = constestName;
+            this.points = points;
+        }
+
+        public void setConstestName(String constestName) {
+            this.constestName = constestName;
+        }
+
+        public void setPoints(int points) {
+            this.points = points;
+        }
+
+        public String getConstestName() {
+            return this.constestName;
+        }
+
+        public int getPoints() {
+            return this.points;
+        }
+    }
+}
