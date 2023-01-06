7f027f1f5768d77ab8300d888b8f84b8b1c3d950
Ivan Pachev <mr.pachev@abv.bg>
2023-01-06 16:03:14 +0200

    Update Judge_02.java

--- JavaFundamentals/MoreExer_06/Judge_02.java
+++ JavaFundamentals/MoreExer_06/Judge_02.java
@@ -28,13 +28,11 @@ public class Judge_02 {
                 usersMap.put(contest, userList);
             } else {                                //курса съществува
                 boolean isExistUser = false;
-                for (Map.Entry<String, List<User>> entry : usersMap.entrySet()) {
 
-                    for (User user1 : entry.getValue()) {
-                        if (user1.getUserName().equals(name) && entry.getKey().equals(contest)) {
-                            isExistUser = true;
-                            break;
-                        }
+                for (User user : usersMap.get(contest)) {
+                    if (user.getUserName().equals(name)) { //проверка дали участника съществува
+                        isExistUser = true;
+                        break;
                     }
                 }
                 if (!isExistUser) {
@@ -49,98 +47,69 @@ public class Judge_02 {
                         }
                     }
                 }
-
-
-
-
-//                boolean isExistUser = false;
-//
-//                for (User user : usersMap.get(contest)) {
-//                    if (user.getUserName().equals(name)) { //проверка дали участника съществува
-//                        isExistUser = true;
-//                        break;
-//                    }
-//                }
-//                if (!isExistUser) {
-//                    User user = new User(name, points);
-//                    userList = usersMap.get(contest);
-//                    userList.add(user);
-//                    usersMap.put(contest, userList);
-//                } else {
-//                    for (User user : usersMap.get(contest)) {
-//                        if (user.getPoints() <= (points) && user.getUserName().equals(name)) { //проверка дали дадените точки са повече от записаните вече
-//                            user.setPoints(points);
-//                        }
-//                    }
-//                }
+            }
+            input = scanner.nextLine();
         }
-        input = scanner.nextLine();
-    }
 
-        usersMap.forEach((k,v)->
-
-    {                                    //принтиране на основния дневник
-        int[] counter = {1};                                        //създваване на брояч вътре в ламбда израза
-        System.out.printf("%s: %d participants%n", k, v.size());    //принтиране на ключа и дължината на стойността
-        v.stream()                                                  //обхождане и сортиране на стойността, която е списък от обекти
-                .sorted((s1, s2) -> s2.getPoints() - s1.getPoints())
-                .forEach(i -> System.out.printf("%d. %s <::> %d%n", counter[0]++, i.getUserName(), i.getPoints()));
-    });
+        usersMap.forEach((k, v) -> {                                    //принтиране на основния дневник
+            int[] counter = {1};                                        //създваване на брояч вътре в ламбда израза
+            System.out.printf("%s: %d participants%n", k, v.size());    //принтиране на ключа и дължината на стойността
+            v.stream()                                                  //обхождане и сортиране на стойността, която е списък от обекти
+                    .sorted((s1, s2) -> s2.getPoints() - s1.getPoints())
+                    .forEach(i -> System.out.printf("%d. %s <::> %d%n", counter[0]++, i.getUserName(), i.getPoints()));
+        });
 
         System.out.println("Individual standings:");
 
-    Map<String, Integer> usersAllPointsMap = new LinkedHashMap<>();
+        Map<String, Integer> usersAllPointsMap = new LinkedHashMap<>();
 
-        for(
-    Map.Entry<String, List<User>> entry :usersMap.entrySet())
+        for (Map.Entry<String, List<User>> entry : usersMap.entrySet()) {
 
-    {
+            entry.getValue().forEach(entry1 -> {                    //обхождаме само value-то на дневника във вид на лист от обекти
+                if (!usersAllPointsMap.containsKey(entry1.getUserName())) {
+                    usersAllPointsMap.put(entry1.getUserName(), entry1.getPoints());
+                } else {
+                    usersAllPointsMap.put(entry1.getUserName(), usersAllPointsMap.get(entry1.getUserName()) + entry1.getPoints());
+                }
+            });
+        }
 
-        entry.getValue().forEach(entry1 -> {                    //обхождаме само value-то на дневника във вид на лист от обекти
-            if (!usersAllPointsMap.containsKey(entry1.getUserName())) {
-                usersAllPointsMap.put(entry1.getUserName(), entry1.getPoints());
-            } else {
-                usersAllPointsMap.put(entry1.getUserName(), usersAllPointsMap.get(entry1.getUserName()) + entry1.getPoints());
-            }
+        Map<String, Integer> finalMap = usersAllPointsMap.entrySet().stream()  //създаване и сортиране на дневник с потребител и общ брой точки в низходящ ред
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new
+                ));
+
+        int[] counter = {1};
+        finalMap.forEach((key, value) ->
+
+        {   //обхождаме финалинят дневник и принтираме
+
+            System.out.printf("%d. %s -> %d%n", counter[0]++, key, value);
+
         });
+
     }
 
-    Map<String, Integer> finalMap = usersAllPointsMap.entrySet().stream()  //създаване и сортиране на дневник с потребител и общ брой точки в низходящ ред
-            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-            .collect(Collectors.toMap(
-                    Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new
-            ));
+    static class User {
+        private final String userName;
+        private int points;
 
-    int[] counter = {1};
-        finalMap.forEach((key,value)->
+        public User(String user, int points) {
+            this.userName = user;
+            this.points = points;
+        }
 
-    {   //обхождаме финалинят дневник и принтираме
+        public void setPoints(int points) {
+            this.points = points;
+        }
 
-        System.out.printf("%d. %s -> %d%n", counter[0]++, key, value);
+        public String getUserName() {
+            return this.userName;
+        }
 
-    });
-
-}
-
-static class User {
-    private final String userName;
-    private int points;
-
-    public User(String user, int points) {
-        this.userName = user;
-        this.points = points;
+        public int getPoints() {
+            return this.points;
+        }
     }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public String getUserName() {
-        return this.userName;
-    }
-
-    public int getPoints() {
-        return this.points;
-    }
-}
 }
