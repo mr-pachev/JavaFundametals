12f6af255659413b6d4c82c707df44e514b26f88
Ivan Pachev <mr.pachev@abv.bg>
2023-01-15 12:53:57 +0200

    MOBAChallenger_03

--- JavaFundamentals/MoreExer_06/MOBAChallenger_03.java
+++ JavaFundamentals/MoreExer_06/MOBAChallenger_03.java
@@ -27,11 +27,11 @@ public class MOBAChallenger_03 {
                     playersDataMap.put(player, currentMap);
                 } else {
 
-                    if (!playersDataMap.get(player).containsKey(position)) {    //проверка дали конкретната позиция НЕ съществува
-                        playersDataMap.get(player).put(position, skill);        //взимане на конкретния играч и добавяне на новата позиция и умение
+                    if (!playersDataMap.get(player).containsKey(position)) {            //проверка дали конкретната позиция НЕ съществува
+                        playersDataMap.get(player).put(position, skill);                //взимане на конкретния играч и добавяне на новата позиция и умение
                     } else {
-                        if (playersDataMap.get(player).get(position) < skill) { //достигане стойността на и на двата речника
-                            playersDataMap.get(player).put(position, skill);    //достигане стойността на основния речник и промяната й
+                        if (playersDataMap.get(player).get(position) < skill) {         //достигане стойността на и на двата речника
+                            playersDataMap.get(player).put(position, skill);            //достигане стойността на основния речник и промяната й
                         }
                     }
                 }
@@ -44,35 +44,68 @@ public class MOBAChallenger_03 {
                     boolean isExist = false;
                     for (String s : playersDataMap.get(firtsPlayer).keySet()) {         //обхожда стойностите на основния речник спрямо ключа firstPlayer
                         for (String s1 : playersDataMap.get(secondPlayer).keySet()) {   //обхожда стойностите на основния речник спрямо ключа secondPlayer
-                            if (s.equals(s1)) {         //проверка дали има обща стойност от тип String спрямо двата ключа
+                            if (s.equals(s1)) {                                         //проверка дали има обща стойност от тип String спрямо двата ключа
                                 isExist = true;
                                 break;
                             }
                         }
                     }
-                    if(isExist){
+                    if (isExist) {
 
-                        if (playersDataMap.get(firtsPlayer).values().stream()
+                        if (playersDataMap.get(firtsPlayer).values().stream()       //взимане сумата но стойностите тип Integer на основния речник спрямо ключ firstPlayer
                                 .mapToInt(i -> i)
                                 .sum()
                                 >
-                                playersDataMap.get(secondPlayer).values().stream()
-                                        .mapToInt(i -> i).sum()) {
-                            playersDataMap.remove(secondPlayer);
+                                playersDataMap.get(secondPlayer).values().stream()  //взимане сумата но стойностите тип Integer на основния речник спрямо ключ secondPlayer
+                                        .mapToInt(i -> i)
+                                        .sum()
+                        ) {
+                            playersDataMap.remove(secondPlayer);                    //премахване на ключа на основния речник с по-малко общи точки от стойността
                         } else if (playersDataMap.get(firtsPlayer).values().stream()
-                                .mapToInt(i -> i).sum()
+                                .mapToInt(i -> i)
+                                .sum()
                                 <
                                 playersDataMap.get(secondPlayer).values().stream()
                                         .mapToInt(i -> i).sum()) {
-                            playersDataMap.remove(firtsPlayer);
+                            playersDataMap.remove(firtsPlayer);                      //премахване на ключа на основния речник с по-малко общи точки от стойността
                         }
                     }
-
                 }
-
             }
             input = scanner.nextLine();
         }
-        System.out.println();
+
+        playersDataMap.entrySet()
+                .stream()
+                .sorted((p1, p2) -> { //сортировка в низходящ ред на основния речник по стойност -> сумата от стойностите на речника-стойност
+                    int result = Integer.compare(p2.getValue().values().stream()
+                                    .mapToInt(i -> i)
+                                    .sum(),
+                            p1.getValue().values().stream()
+                                    .mapToInt(i -> i)
+                                    .sum());
+                    if (result == 0) { //при равенство на сортировка в низходящ ред на основния речник по стойност -> сумата от стойностите на речника-стойност
+                        result = p1.getKey().compareTo(p2.getKey());   //сортира основния речник по ключ във възходящ ред
+                    }
+                    return result;
+                })
+                .forEach(entry -> {
+                    System.out.printf("%s: %d skill%n", entry.getKey(), entry.getValue().values().stream()
+                                                                                                .mapToInt(i -> i)
+                                                                                                .sum());
+                    entry.getValue()                                    //обхождане на втория ребник
+                            .entrySet()
+                            .stream()
+                            .sorted((e1, e2) -> {
+                                int res = Integer.compare(e2.getValue(), e1.getValue());    //сортиране по низходящ ред на стойностите от тип Integer от втория речник
+                                if (res == 0) {                                             //при равенство от сортирането по низходящ ред на стойностите от тип Integer от втория речник
+                                    res = e1.getKey().compareTo(e2.getKey());               //сортиране във възходящ ред на ключа от втория речник-позицията
+                                }
+                                return res;
+                            }).forEach(e -> {
+                                System.out.printf("- %s <::> %d%n", e.getKey(), e.getValue());
+                            });
+                });
+
     }
 }
