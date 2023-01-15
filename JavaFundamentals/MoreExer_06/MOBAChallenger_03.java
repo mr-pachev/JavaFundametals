ec8536f338a7fe002ea5aebaac835e7a72827900
Ivan Pachev <mr.pachev@abv.bg>
2023-01-15 10:08:37 +0200

    Update MOBAChallenger_03.java

--- JavaFundamentals/MoreExer_06/MOBAChallenger_03.java
+++ JavaFundamentals/MoreExer_06/MOBAChallenger_03.java
@@ -17,32 +17,45 @@ public class MOBAChallenger_03 {
 
         while (!input.equals("Season end")) {
 
-            String player = input.split(" -> ")[0];
-            String position = input.split(" -> ")[1];
-            int skill = Integer.parseInt(input.split(" -> ")[2]);
+            if (input.contains(" -> ")) {
+                String player = input.split(" -> ")[0];
+                String position = input.split(" -> ")[1];
+                int skill = Integer.parseInt(input.split(" -> ")[2]);
 
-            Player player1 = new Player(position, skill);
-            List<Player> playerDataList = new ArrayList<>();
+                Player player1 = new Player(position, skill);
+                List<Player> playerDataList = new ArrayList<>();
 
-            if (!playersDataMap.containsKey(player)) {           //проверка дали играча съществува
-                playerDataList.add(player1);
-                playersDataMap.put(player, playerDataList);
-            } else {
+                if (!playersDataMap.containsKey(player)) {                   //проверка дали играча съществува
+                    playerDataList.add(player1);
+                    playersDataMap.put(player, playerDataList);
+                } else {
 
-                for (Player player2 : playersDataMap.get(player)) {
-                    if (!player2.getPosition().equals(position)) {       //проверка дали коткретната позиция на дадения играч не съществува
-                        player1 = new Player(position, skill);           //създаване на нов обект с новата позиция и умения
-                        playerDataList = playersDataMap.get(player);     //взимане на текущата стойност-списък на речника
-                        playerDataList.add(player1);                     //добавяне на новата позиция и умение към конкретния играч
-                        playersDataMap.put(player, playerDataList);
-                        break;
-                    } else if (player2.getSkill() <= skill) {
-                        player2.setSkill(skill);
-                        break;
+                    for (Player player2 : playersDataMap.get(player)) {
+                        if (!player2.getPosition().equals(position)) {       //проверка дали коткретната позиция на дадения играч не съществува
+                            player1 = new Player(position, skill);           //създаване на нов обект с новата позиция и умения
+                            playerDataList = playersDataMap.get(player);     //взимане на текущата стойност-списък на речника
+                            playerDataList.add(player1);                     //добавяне на новата позиция и умение към конкретния играч
+                            playersDataMap.put(player, playerDataList);
+                            break;
+                        } else if (player2.getSkill() <= skill) {
+                            player2.setSkill(skill);
+                            break;
+                        }
                     }
                 }
+            }else if (input.contains(" vs ")){
+                String playerOne = input.split(" ")[0];
+                String playerTwoo = input.split(" ")[2];
+
+               if(!playersDataMap.containsKey(playerOne) && !playersDataMap.containsKey(playerTwoo)){ //проверка дали двамата играчи съществуват в речника
+                   break;
+               }else {
+
+
+               }
+
             }
-            input = scanner.nextLine();
+          input = scanner.nextLine();
         }
     }
 
