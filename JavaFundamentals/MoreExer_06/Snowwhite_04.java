b372e1d419be46452a675c174c8f7d72c184dc96
Ivan Pachev <mr.pachev@abv.bg>
2023-01-15 13:53:04 +0200

    Update Snowwhite_04.java

--- JavaFundamentals/MoreExer_06/Snowwhite_04.java
+++ JavaFundamentals/MoreExer_06/Snowwhite_04.java
@@ -13,7 +13,7 @@ public class Snowwhite_04 {
         Map<String, Map<String, Integer>> dwarfDataMap = new LinkedHashMap<>();   //речник съдържащ цвета на шаппката, речник с името и физиката на джуджето
 
 
-        while (!input.equals("Season end")) {
+        while (!input.equals("Once upon a time")) {
             String dwarfName = input.split(" <:> ")[0];                     //име на джуджето
             String dwarfHatColor = input.split(" <:> ")[1];                 //цвят на шапката на джуджето
             int dwarfPhysics = Integer.parseInt(input.split(" <:> ")[2]);   // физика на джуджето
@@ -21,10 +21,16 @@ public class Snowwhite_04 {
             Map<String, Integer> currentMap = new LinkedHashMap<>();              //речник съдържащ името и физиката на джуджето
             currentMap.put(dwarfName, dwarfPhysics);
 
-                if(!dwarfDataMap.containsKey(dwarfHatColor)){                     //проверка дали НЯМА такъв цвят шапка
-                    dwarfDataMap.put(dwarfHatColor, currentMap);
-            }else {
-
+            if (!dwarfDataMap.containsKey(dwarfHatColor)) {                       //проверка дали НЯМА такъв цвят шапка
+                dwarfDataMap.put(dwarfHatColor, currentMap);
+            } else {
+                if(dwarfDataMap.get(dwarfHatColor).containsKey(dwarfName)){        //проверка дали И името на конкретното джуджетата го има в речника
+                    if(dwarfDataMap.get(dwarfHatColor).get(dwarfName) < dwarfPhysics){
+                        dwarfDataMap.get(dwarfHatColor).put(dwarfName, dwarfPhysics);
+                    }
+                }else {
+                    dwarfDataMap.get(dwarfHatColor).put(dwarfName, dwarfPhysics);
+                }
             }
 
             input = scanner.nextLine();
