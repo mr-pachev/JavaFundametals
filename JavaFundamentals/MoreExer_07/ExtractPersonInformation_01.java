b09d6a52d4a121f19b3beb7ff38c1ce3ed4cced4
Ivan Pachev <mr.pachev@abv.bg>
2023-01-16 19:45:40 +0200

    Create ExtractPersonInformation_01.java

---
+++ JavaFundamentals/MoreExer_07/ExtractPersonInformation_01.java
@@ -0,0 +1,13 @@
+package MoreExer_07;
+
+import java.util.Scanner;
+
+public class ExtractPersonInformation_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        
+    }
+}
