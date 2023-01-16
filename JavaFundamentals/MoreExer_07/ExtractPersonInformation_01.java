ece7f2de156c04bf8fbb8f3c8de6cb0a2c17405b
Ivan Pachev <mr.pachev@abv.bg>
2023-01-16 20:11:30 +0200

    Update ExtractPersonInformation_01.java

--- JavaFundamentals/MoreExer_07/ExtractPersonInformation_01.java
+++ JavaFundamentals/MoreExer_07/ExtractPersonInformation_01.java
@@ -6,8 +6,25 @@ public class ExtractPersonInformation_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        StringBuilder resultName = new StringBuilder();
+        StringBuilder resultAge = new StringBuilder();
 
-        
+        for (int row = 1; row <= n; row++) {
+
+            String[] personDataArr = scanner.nextLine().split("[@|#*]+");
+            String name = personDataArr[1];
+            String age = personDataArr[3];
+
+            resultName.append(name);
+            resultAge.append(age);
+
+            System.out.println(resultName + " is " + resultAge + " years old.");
+
+            resultName.delete(0, resultName.length());
+            resultAge.delete(0, resultAge.length());
+        }
+
     }
 }
