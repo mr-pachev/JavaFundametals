8d90cbdb2c0b76fd07b2a352c68f593488a3756b
Ivan Pachev <mr.pachev@abv.bg>
2023-01-23 21:40:44 +0200

    Create HTML_05.java

---
+++ JavaFundamentals/MoreExer_07/HTML_05.java
@@ -0,0 +1,31 @@
+package MoreExer_07;
+
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HTML_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String articleName = scanner.nextLine();
+        String articleContains = scanner.nextLine();
+        String input = scanner.nextLine();;
+
+        List<String> comment = new ArrayList<>();
+
+        while (!input.equals("end of comments")) {
+            comment.add(input);
+
+            input =scanner.nextLine();
+        }
+
+        System.out.printf("<h1>%n   %s%n</h1>%n", articleName);
+
+        for (int i = 0; i < comment.size(); i++) {
+            System.out.printf("<div>%n   %s%n</div>", comment.get(i));
+        }
+    }
+}
