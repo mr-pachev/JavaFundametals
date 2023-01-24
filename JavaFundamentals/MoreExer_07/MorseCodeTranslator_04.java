17f5b853ee19e7a7c9a530cc2d04149f0da1a4e4
Ivan Pachev <mr.pachev@abv.bg>
2023-01-24 15:45:06 +0200

    Update MorseCodeTranslator_04.java

--- JavaFundamentals/MoreExer_07/MorseCodeTranslator_04.java
+++ JavaFundamentals/MoreExer_07/MorseCodeTranslator_04.java
@@ -7,37 +7,42 @@ public class MorseCodeTranslator_04 {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        String[] personDataArr = input.split("\\|");
 
-        Map<String, Character> mourseMap = new LinkedHashMap<>();
-        mourseMap.put(".-", 'A');
-        mourseMap.put("-...", 'B');
-        mourseMap.put("-.-.", 'C');
-        mourseMap.put("-..", 'D');
-        mourseMap.put(".", 'E');
-        mourseMap.put("..-.", 'F');
-        mourseMap.put("--.", 'G');
-        mourseMap.put("....", 'H');
-        mourseMap.put("..", 'I');
-        mourseMap.put(".---", 'J');
-        mourseMap.put("-.-", 'K');
-        mourseMap.put(".-..", 'L');
-        mourseMap.put("--", 'M');
-        mourseMap.put("-.", 'N');
-        mourseMap.put("---", 'O');
-        mourseMap.put(".--.", 'P');
-        mourseMap.put("--.-", 'Q');
-        mourseMap.put(".-.", 'R');
-        mourseMap.put("...", 'S');
-        mourseMap.put("-", 'T');
-        mourseMap.put("..-", 'U');
-        mourseMap.put("...-", 'V');
-        mourseMap.put(".--", 'W');
-        mourseMap.put("-..-", 'X');
-        mourseMap.put("-.--", 'Y');
-        mourseMap.put("--..", 'Z');
+        Map<String, String> mourseMap = new LinkedHashMap<>();
+        mourseMap.put(".-", "A");
+        mourseMap.put("-...", "B");
+        mourseMap.put("-.-.", "C");
+        mourseMap.put("-..", "D");
+        mourseMap.put(".", "E");
+        mourseMap.put("..-.", "F");
+        mourseMap.put("--.", "G");
+        mourseMap.put("....", "H");
+        mourseMap.put("..", "I");
+        mourseMap.put(".---", "J");
+        mourseMap.put("-.-", "K");
+        mourseMap.put(".-..", "L");
+        mourseMap.put("--", "M");
+        mourseMap.put("-.", "N");
+        mourseMap.put("---", "O");
+        mourseMap.put(".--.", "P");
+        mourseMap.put("--.-", "Q");
+        mourseMap.put(".-.", "R");
+        mourseMap.put("...", "S");
+        mourseMap.put("-", "T");
+        mourseMap.put("..-", "U");
+        mourseMap.put("...-", "V");
+        mourseMap.put(".--", "W");
+        mourseMap.put("-..-", "X");
+        mourseMap.put("-.--", "Y");
+        mourseMap.put("--..", "Z");
+
+       List<String> mourseCode = Arrays.stream(input.split("\\|")).toList();
 
 
+        String result = "";
+        for (String symbol : mourseCode){
+            result += mourseMap.get(symbol);
+        }
         System.out.println();
 
     }
