569032e19b725109ad584c18c41404021e8e380a
Ivan Pachev <mr.pachev@abv.bg>
2023-01-25 09:25:33 +0200

    Update TreasureFinder_03.java

--- JavaFundamentals/MoreExer_07/TreasureFinder_03.java
+++ JavaFundamentals/MoreExer_07/TreasureFinder_03.java
@@ -7,31 +7,33 @@ public class TreasureFinder_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] key = scanner.nextLine().split(" "); //вход ключ
-        String input = scanner.nextLine();                 //текс за декриптиране
-        String result = "";
+        String[] key = scanner.nextLine().split(" ");     //вход ключ
+        String input = scanner.nextLine();                      //текс за декриптиране
+        String result = "";                                     //ще пази откритите символи и интервали за печат
 
         while (!input.equals("find")){
 
             result = "";
-            char[] symbols = input.toCharArray();
+            char[] symbols = input.toCharArray();               //входния низ се конвертира в масив от символи
             int count = 0;
 
             for (int i = 0; i < symbols.length; i++) {
-                int symbolNum = (int) symbols[i];
+                int symbolNum = (int) symbols[i];               //взимане на номера от ASCII таблицата на текущия символ от низа
 
-                if(count < key.length) {
-                    symbolNum -= Integer.parseInt(key[count]);
+                if(count < key.length) {                        //проверка осигуряваща превъртането на ключа по дължина на целия входящ низ
+                    symbolNum -= Integer.parseInt(key[count]);  //изваждане стойността на ключа от номера в ASCII таблицата на текущия символ
                 }else {
                     count = 0;
                     symbolNum -= Integer.parseInt(key[count]);
                 }
                 count++;
 
-                char currentSymbol = (char) symbolNum;
+                char currentSymbol = (char) symbolNum;          //превръщане от int стойност в символ
                 result+=currentSymbol;
             }
-            System.out.printf("Found %s at %s%n.", );
+            String type = result.split("[\\&\\>]")[1];
+            String point = result.split("[\\<\\>]")[1];
+            System.out.printf("Found %s at %s%n", type, point);
             input = scanner.nextLine();
         }
 
