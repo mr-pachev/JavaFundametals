c09a260f3c5bbcf89fb4a3d3ec697a33f185b529
Ivan Pachev <mr.pachev@abv.bg>
2023-02-06 09:30:51 +0200

    Create WinningTicket_01.java

---
+++ JavaFundamentals/MoreExer_08/WinningTicket_01.java
@@ -0,0 +1,16 @@
+package MoreExer_08;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class WinningTicket_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] wordsArr = input.split(",\s+");
+
+        System.out.println();
+        
+    }
+}
