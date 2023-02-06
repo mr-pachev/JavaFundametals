5c19dcd0ca149d39d7f1f937479e142731b242a2
Ivan Pachev <mr.pachev@abv.bg>
2023-02-06 14:33:28 +0200

    Update WinningTicket_01.java

--- JavaFundamentals/MoreExer_08/WinningTicket_01.java
+++ JavaFundamentals/MoreExer_08/WinningTicket_01.java
@@ -1,6 +1,13 @@
 package MoreExer_08;
 
+import com.sun.jdi.Value;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WinningTicket_01 {
     public static void main(String[] args) {
@@ -8,41 +15,45 @@ public class WinningTicket_01 {
 
         String input = scanner.nextLine();                              //входни данни
         String[] wordsArr = input.split("(\\s+)*,(\\s+)*");       //масив с думи за проверка
+        
+        Map<String, Integer> symbolSumMap = new LinkedHashMap<>();
 
         for (int numWords = 0; numWords < wordsArr.length; numWords++) {
             char[] currentWord = wordsArr[numWords].toCharArray();      //символите на конкретната дума масив
 
             if (isExistSumChar(currentWord)) {                          //проверка дали думата съдържа 20 символа
-                if (isWinSymbol(currentWord)) {                         //проверка дали съдържа печеливш символ
-                    int counterLeft = 0;
-                    int counterRight = 0;
 
-                    char winSymbol = isCurrentWinSymbol(currentWord);
+                Pattern pattern = Pattern.compile("[@$#\\^]+");
+                Matcher mather = pattern.matcher(Arrays.toString(currentWord));
 
-                    for (int i = 0; i < currentWord.length / 2; i++) {  //проверка колко пъти печелившия символ се повтаря вляво
-                        if (currentWord[i] == winSymbol) {
-                            counterLeft++;
-                        }
-                    }
-                    for (int i = currentWord.length - 1; i > currentWord.length / 2; i--) { //проверка колко пъти печелившия символ се повтаря вдясно
-                        if (currentWord[i] == winSymbol) {
-                            counterRight++;
-                        }
-                    }
+                symbolSumMap.put("@", 0);
+                symbolSumMap.put("#", 0);
+                symbolSumMap.put("$", 0);
+                symbolSumMap.put("^", 0);
 
-                    if (counterLeft == counterRight && counterLeft >= 6 && counterLeft < 10){
-                        System.out.printf("ticket \"%s\" - %d%c%n", wordsArr[numWords], counterLeft, winSymbol);
-                    }else if(counterLeft == 10){
-                        System.out.printf("ticket \"%s\" - %d%c Jackpot!%n", wordsArr[numWords], counterLeft, winSymbol);
-                    }else {
-                        System.out.printf("ticket \"%s\" - no match", wordsArr[numWords]);
+                while (mather.find()){
+                    String currentSymbol = mather.group();
+                    switch (currentSymbol){
+                        case "@":
+                            symbolSumMap.put("@", symbolSumMap.get("@") + 1);
+                            break;
+                        case "#":
+                            symbolSumMap.put("#", symbolSumMap.get("#") + 1);
+                            break;
+                        case "$":
+                            symbolSumMap.put("$", symbolSumMap.get("$") + 1);
+                            break;
+                        case "^":
+                            symbolSumMap.put("^", symbolSumMap.get("^") + 1);
+                            break;
                     }
-                } else {
-                    System.out.printf("ticket \"%s\" - no match", wordsArr[numWords]);
+                    System.out.println();
                 }
+
             } else {
                 System.out.println("invalid ticket");
             }
+            System.out.println();
         }
 
     }
@@ -50,35 +61,6 @@ public class WinningTicket_01 {
     //проверка дали думата съдържа 20 символа
     static boolean isExistSumChar(char[] currentSymbol) {
         return currentSymbol.length == 20;
-    }
-
-    //проверка дали съдържа печеливш символ
-    static boolean isWinSymbol(char[] currentSymbol) {
-        boolean isExist = false;
-        for (char e : currentSymbol) {
-            if (e == '@' || e == '#' || e == '$' || e == '^') {
-                isExist = true;
-                break;
-            }
-        }
-        return isExist;
-    }
-
-    //проверка кой печеливш символ съдържа думата
-    static char isCurrentWinSymbol(char[] currentSymbol) {
-        char winSymbol = 0;
-        for (char c : currentSymbol) {
-            if (c == '@') {
-                winSymbol = c;
-            } else if (c == '#') {
-                winSymbol = c;
-            } else if (c == '$') {
-                winSymbol = c;
-            } else if (c == '^') {
-                winSymbol = c;
-            }
-        }
-        return winSymbol;
     }
 
 }
