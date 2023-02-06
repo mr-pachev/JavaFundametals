34684c91a8d0ea476d643f1f9ad4be52934e9770
Ivan Pachev <mr.pachev@abv.bg>
2023-02-06 11:32:30 +0200

    Update WinningTicket_01.java

--- JavaFundamentals/MoreExer_08/WinningTicket_01.java
+++ JavaFundamentals/MoreExer_08/WinningTicket_01.java
@@ -12,11 +12,52 @@ public class WinningTicket_01 {
 
         for (int numWords = 0; numWords < wordsArr.length; numWords++) {
             char[] currentWord = wordsArr[numWords].toCharArray();      //символите на конкретната дума масив
-            System.out.println();
+
+            if (isExistSumChar(currentWord)) {                          //проверка дали думата съдържа 20 символа
+                if (isWinSymbol(currentWord)) {                         //проверка дали съдържа печеливш символ
+                    System.out.println();
+                } else {
+                    System.out.printf("ticket \"%s\" - no match", wordsArr[numWords]);
+                }
+            } else {
+                System.out.println("invalid ticket");
+            }
         }
 
-
-
-        
     }
+
+    //проверка дали думата съдържа 20 символа
+    static boolean isExistSumChar(char[] currentSymbol) {
+        return currentSymbol.length == 20;
+    }
+
+    //проверка дали съдържа печеливш символ
+    static boolean isWinSymbol(char[] currentSymbol) {
+        boolean isExist = false;
+        for (char e : currentSymbol) {
+            if (e == '@' || e == '#' || e == '$' || e == '^') {
+                isExist = true;
+                break;
+            }
+        }
+        return isExist;
+    }
+
+    //проверка кой печеливш символ съдържа думата
+    static char isWinSymbolExist(char[] currentSymbol) {
+        char winSymbol = 0;
+        for (char c : currentSymbol) {
+            if (c == '@') {
+                winSymbol = c;
+            } else if (c == '#') {
+                winSymbol = c;
+            } else if (c == '$') {
+                winSymbol = c;
+            } else if (c == '^') {
+                winSymbol = c;
+            }
+        }
+        return winSymbol;
+    }
+
 }
