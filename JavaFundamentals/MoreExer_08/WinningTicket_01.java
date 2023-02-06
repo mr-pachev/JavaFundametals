cfe4cd622d2ff1cd89a66c663a5976fa0f01ae8d
Ivan Pachev <mr.pachev@abv.bg>
2023-02-06 15:51:33 +0200

    Update WinningTicket_01.java

--- JavaFundamentals/MoreExer_08/WinningTicket_01.java
+++ JavaFundamentals/MoreExer_08/WinningTicket_01.java
@@ -2,12 +2,14 @@ package MoreExer_08;
 
 import com.sun.jdi.Value;
 
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WinningTicket_01 {
     public static void main(String[] args) {
@@ -15,47 +17,84 @@ public class WinningTicket_01 {
 
         String input = scanner.nextLine();                              //входни данни
         String[] wordsArr = input.split("(\\s+)*,(\\s+)*");       //масив с думи за проверка
-        
-        Map<String, Integer> symbolSumMap = new LinkedHashMap<>();
+
+        Map<String, Integer> symbolSumMapLeft = new LinkedHashMap<>();
+        Map<String, Integer> symbolSumMapRight = new LinkedHashMap<>();
 
         for (int numWords = 0; numWords < wordsArr.length; numWords++) {
             char[] currentWord = wordsArr[numWords].toCharArray();      //символите на конкретната дума масив
+            symbolSumMapLeft.put("#", 0);
+            symbolSumMapLeft.put("@", 0);
+            symbolSumMapLeft.put("$", 0);
+            symbolSumMapLeft.put("^", 0);
+            symbolSumMapRight.put("@", 0);
+            symbolSumMapRight.put("#", 0);
+            symbolSumMapRight.put("$", 0);
+            symbolSumMapRight.put("^", 0);
+
 
             if (isExistSumChar(currentWord)) {                          //проверка дали думата съдържа 20 символа
-
-                Pattern pattern = Pattern.compile("[@$#\\^]+");
-                Matcher mather = pattern.matcher(Arrays.toString(currentWord));
-
-                symbolSumMap.put("@", 0);
-                symbolSumMap.put("#", 0);
-                symbolSumMap.put("$", 0);
-                symbolSumMap.put("^", 0);
-
-                while (mather.find()){
-                    String currentSymbol = mather.group();
-                    switch (currentSymbol){
-                        case "@":
-                            symbolSumMap.put("@", symbolSumMap.get("@") + 1);
-                            break;
-                        case "#":
-                            symbolSumMap.put("#", symbolSumMap.get("#") + 1);
-                            break;
-                        case "$":
-                            symbolSumMap.put("$", symbolSumMap.get("$") + 1);
-                            break;
-                        case "^":
-                            symbolSumMap.put("^", symbolSumMap.get("^") + 1);
-                            break;
+                boolean isExist = false;
+                for (int i = 0; i < currentWord.length / 2; i++) {
+                    String currentSymbol = String.valueOf(currentWord[i]);
+                    if (symbolSumMapLeft.containsKey(currentSymbol)){
+                        symbolSumMapLeft.put(currentSymbol, symbolSumMapLeft.get(currentSymbol) + 1);
+                        isExist = true;
                     }
-                    System.out.println();
+                }
+
+                for (int i = currentWord.length - 1; i >= currentWord.length / 2; i--) {
+                    String currentSymbol = String.valueOf(currentWord[i]);
+                    if (symbolSumMapRight.containsKey(currentSymbol)){
+                        symbolSumMapRight.put(currentSymbol, symbolSumMapRight.get(currentSymbol) + 1);
+                        isExist = true;
+                    }
+                }
+
+                if (!isExist){
+                    System.out.printf("ticket \"%s\" - no match", wordsArr[numWords]);
+                    continue;
+                }
+
+                symbolSumMapLeft = symbolSumMapLeft.entrySet().stream()
+                        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                symbolSumMapRight = symbolSumMapRight.entrySet().stream()
+                        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                String winSymbolLeft = "";
+                int repeatingSymbolLeft = 0;
+                for(Map.Entry<String, Integer> entry : symbolSumMapLeft.entrySet()) {
+                    winSymbolLeft = entry.getKey();
+                    repeatingSymbolLeft = entry.getValue();
+                    break;
+                }
+
+                String winSymbolRight = "";
+                int repeatingSymbolRight = 0;
+                for(Map.Entry<String, Integer> entry : symbolSumMapRight.entrySet()) {
+                    winSymbolRight = entry.getKey();
+                    repeatingSymbolRight = entry.getValue();
+                    break;
+                }
+
+                if (winSymbolLeft.equals(winSymbolRight) && repeatingSymbolLeft == repeatingSymbolRight){
+                    if (repeatingSymbolLeft >= 6 && repeatingSymbolLeft < 10){
+                        System.out.printf("ticket \"%s\" - %d%s%n", wordsArr[numWords], repeatingSymbolLeft, winSymbolLeft);
+                    }else if (repeatingSymbolLeft == 10){
+                        System.out.printf("ticket \"%s\" - %d%s Jackpot!%n", wordsArr[numWords], repeatingSymbolLeft, winSymbolLeft);
+                    }else {
+                        System.out.printf("ticket \"%s\" - no match", wordsArr[numWords]);
+                    }
                 }
 
             } else {
                 System.out.println("invalid ticket");
             }
-            System.out.println();
-        }
 
+        }
     }
 
     //проверка дали думата съдържа 20 символа
