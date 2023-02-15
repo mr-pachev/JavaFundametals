6e660b37de590f36c72459572d53c3f659a46603
Ivan Pachev <mr.pachev@abv.bg>
2023-02-15 21:41:41 +0200

    Update PostOffice_03.java

--- JavaFundamentals/MoreExer_08/PostOffice_03.java
+++ JavaFundamentals/MoreExer_08/PostOffice_03.java
@@ -1,12 +1,17 @@
 package MoreExer_08;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class PostOffice_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split("\\|"); //входни данни
+
+        Pattern lettersPattern = Pattern.compile("([#\\$%\\*&])(?<letters>[A-Z]+)(?:\\1)"); //regex за главните букви
 
 
+        System.out.println();
     }
 }
