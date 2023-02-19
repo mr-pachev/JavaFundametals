08066964021a9cba1d5fffbc8ea6240802b55a2b
Ivan Pachev <mr.pachev@abv.bg>
2023-02-19 18:55:59 +0200

    Create TheImitationGame_01.java

---
+++ JavaFundamentals/FinalExams/TheImitationGame_01.java
@@ -0,0 +1,11 @@
+package FinalExams;
+
+import java.util.Scanner;
+
+public class TheImitationGame_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
