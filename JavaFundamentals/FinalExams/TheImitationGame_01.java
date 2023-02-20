e0a73fee2ba533ea0ceb3827af2b0bee6fd5781c
Ivan Pachev <mr.pachev@abv.bg>
2023-02-20 20:58:14 +0200

    Update TheImitationGame_01.java

--- JavaFundamentals/FinalExams/TheImitationGame_01.java
+++ JavaFundamentals/FinalExams/TheImitationGame_01.java
@@ -1,11 +1,40 @@
 package FinalExams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TheImitationGame_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        String coded = scanner.nextLine();
+        List<Character> codedMessageList = new ArrayList<>(); //масив от кодираната дума
+
+        for (Character s : coded.toCharArray()){
+            codedMessageList.add(s);
+        }
+
+        String input = scanner.nextLine();
+
+        while(!input.equals("Decode")){
+
+            String[] commands = input.split("\\|"); //масив с командите
+
+            if (commands.length == 2){
+
+
+
+
+            }else if (commands.length == 3){
+
+            }
+
+            input = scanner.next();
+        }
+
+
+
     }
 }
