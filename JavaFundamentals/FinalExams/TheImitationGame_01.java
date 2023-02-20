c29c17af5c5fed36cc56cb1609bab03022453b8f
Ivan Pachev <mr.pachev@abv.bg>
2023-02-20 22:13:33 +0200

    Update TheImitationGame_01.java

--- JavaFundamentals/FinalExams/TheImitationGame_01.java
+++ JavaFundamentals/FinalExams/TheImitationGame_01.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import static MoreExerCompanyRoster_01.Main.isNumeric;
 
@@ -11,15 +12,17 @@ public class TheImitationGame_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> codedMessageList = new ArrayList<>(List.of(scanner.nextLine().split(""))); //списък от кодираната дума
+        String incoming = scanner.nextLine();
+        List<String> codedMessageList = Arrays.stream(incoming.split("")).collect(Collectors.toList());
 
         String input = scanner.nextLine();
+        StringBuilder decodeWord = new StringBuilder();
 
-        while(!input.equals("Decode")){
+        while (!input.equals("Decode")) {
 
             String[] commands = input.split("\\|"); //масив с командите
 
-            if (commands.length == 2){                              //преместване на елементи от началото вкрая
+            if (commands.length == 2) {                              //преместване на елементи от началото вкрая
                 int num = Integer.parseInt(commands[1]);
                 for (int i = 0; i < num; i++) {
                     String current = codedMessageList.get(0);
@@ -27,46 +30,46 @@ public class TheImitationGame_01 {
                     codedMessageList.add(current);
                 }
 
-            }else if (commands.length == 3) {
+            } else if (commands.length == 3) {
 
                 if (isNumeric(commands[1])) {                           //вмъкване на елемент
                     int index = Integer.parseInt(commands[1]);
                     String current = commands[2];
 
-                    for (int i = 0; i < current.length() ; i++) {
+                    for (int i = 0; i < current.length(); i++) {
                         String symbol = current.split("")[i];
                         codedMessageList.add(index, symbol);
                         index++;
                     }
 
-                }else {                                                 //заместване на символ
+                } else {                                                 //заместване на символ
                     String oldLetter = commands[1];
                     String newLetter = commands[2];
 
-                    for(int i = 0; i < codedMessageList.size(); i++){
-                        if (oldLetter.equals(codedMessageList.get(i))){
+                    for (int i = 0; i < codedMessageList.size(); i++) {
+                        if (oldLetter.equals(codedMessageList.get(i))) {
                             codedMessageList.set(i, newLetter);
                         }
                     }
                 }
-
             }
 
             input = scanner.next();
         }
 
-        System.out.print("The decrypted message is: ");
-        for (String s : codedMessageList){
-            System.out.print(s);
+        for (String s : codedMessageList) {
+            decodeWord.append(s);
         }
-
+        System.out.print("The decrypted message is: ");
+        System.out.print(decodeWord);
     }
+
     //метод за проверка дали String-a е число
     public static boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
             return true;
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
