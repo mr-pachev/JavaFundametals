1abe214cacba8713e2de9caa6697a207c1b74a4d
Ivan Pachev <mr.pachev@abv.bg>
2023-02-23 16:57:19 +0200

    Create ThePianist_03.java

---
+++ JavaFundamentals/FinalExams/ThePianist_03.java
@@ -0,0 +1,12 @@
+package FinalExams;
+
+import java.util.Scanner;
+
+public class ThePianist_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        
+    }
+}
