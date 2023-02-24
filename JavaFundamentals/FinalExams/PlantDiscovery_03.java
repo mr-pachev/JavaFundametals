22185de163889dd72e7939961ff903cf2f7e1e90
Ivan Pachev <mr.pachev@abv.bg>
2023-02-24 19:01:35 +0200

    Update PlantDiscovery_03.java

--- JavaFundamentals/FinalExams/PlantDiscovery_03.java
+++ JavaFundamentals/FinalExams/PlantDiscovery_03.java
@@ -7,14 +7,14 @@ public class PlantDiscovery_03 {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-        Map<String, List<Integer>> plantsInfo = new LinkedHashMap<>();
+        Map<String, List<Double>> plantsInfo = new LinkedHashMap<>();
 
         for (int i = 0; i < n; i++) {
             String input = scanner.nextLine();
 
-            List<Integer> plantsList = new ArrayList<>();
+            List<Double> plantsList = new ArrayList<>();
             String name = input.split("<->")[0];
-            int rarity = Integer.parseInt(input.split("<->")[1]);
+            double rarity = Double.parseDouble(input.split("<->")[1]);
             plantsList.add(rarity);
             plantsInfo.put(name, plantsList);
         }
@@ -28,10 +28,10 @@ public class PlantDiscovery_03 {
 
                 case "Rate": {
                     String currentPlant = info.split(" - ")[0];
-                    int rating = Integer.parseInt(info.split(" - ")[1]);
+                    double rating = Double.parseDouble(info.split(" - ")[1]);
 
                     if (isExist(currentPlant, plantsInfo)) {
-                        List<Integer> plantsList = plantsInfo.get(currentPlant);
+                        List<Double> plantsList = plantsInfo.get(currentPlant);
 
                         plantsList.add(rating);
                         plantsInfo.put(currentPlant, plantsList);
@@ -42,10 +42,10 @@ public class PlantDiscovery_03 {
                 }
                 case "Update": {
                     String currentPlant = info.split(" - ")[0];
-                    int rarity = Integer.parseInt(info.split(" - ")[1]);
+                    double rarity = Integer.parseInt(info.split(" - ")[1]);
 
                     if (isExist(currentPlant, plantsInfo)) {
-                        List<Integer> plantsList = plantsInfo.get(currentPlant);
+                        List<Double> plantsList = plantsInfo.get(currentPlant);
                         plantsList.set(0, rarity);
                         plantsInfo.put(currentPlant, plantsList);
                     } else {
@@ -56,7 +56,7 @@ public class PlantDiscovery_03 {
                 case "Reset": {
 
                     if (isExist(info, plantsInfo)) {
-                        List<Integer> plantsList = plantsInfo.get(info);
+                        List<Double> plantsList = plantsInfo.get(info);
                         plantsList.remove(1);
                         plantsInfo.put(info, plantsList);
                     } else {
@@ -69,34 +69,51 @@ public class PlantDiscovery_03 {
         }
         System.out.println("Plants for the exhibition:");
 
-        for (Map.Entry<String, List<Integer>> entry : plantsInfo.entrySet()) {
-            System.out.printf("- %s; ", entry.getKey());
+        for (Map.Entry<String, List<Double>> entry : plantsInfo.entrySet()) {
 
-            for (int i = 0; i < entry.getValue().size(); i++) {
-                if (entry.getValue().size() == 1) {
-                    System.out.printf("Rarity: %d; Rating: 0.00%n", entry.getValue().get(0));
-                } else {
-                    List<Integer> average = entry.getValue();
-                    int rarity = entry.getValue().get(0);
+            for (Double s : entry.getValue()) {
+                if (entry.getValue().size() > 1) {
+                    List<Double> average = entry.getValue();
+                    double rarity = entry.getValue().get(0);
                     average.remove(0);
                     double avr = getAverage(average);
-                    System.out.printf("Rarity: %d; Rating: %.2f%n", rarity, avr);
+                    List<Double> newList = new ArrayList<>();
+                    newList.add(rarity);
+                    newList.add(avr);
+                    plantsInfo.put(entry.getKey(), newList);
                     break;
+                }else {
+                    List<Double> newList = new ArrayList<>();
+                    double rarity = entry.getValue().get(0);
+                    newList.add(rarity);
+                    newList.add(0.00);
+                    plantsInfo.put(entry.getKey(), newList);
                 }
             }
         }
 
+        for (Map.Entry<String, List<Double>> entry : plantsInfo.entrySet()) {
+            System.out.printf("- %s; ", entry.getKey());
+
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                if (i == 0) {
+                    System.out.printf("Rarity: %.0f; ", entry.getValue().get(0));
+                } else if (i == 1) {
+                    System.out.printf("Rating: %.2f%n", entry.getValue().get(1));
+                }
+            }
+        }
 
     }
 
-    public static boolean isExist(String plant, Map<String, List<Integer>> plantsMap) {
+    public static boolean isExist(String plant, Map<String, List<Double>> plantsMap) {
         return plantsMap.containsKey(plant);
     }
 
     //метод за намиране на средна стойност на елевентите от List<Integer>
-    private static double getAverage(List<Integer> list) {
-        IntSummaryStatistics stats = list.stream()
-                .mapToInt(Integer::intValue)
+    private static double getAverage(List<Double> list) {
+        DoubleSummaryStatistics stats = list.stream()
+                .mapToDouble(Double::intValue)
                 .summaryStatistics();
         return stats.getAverage();
     }
