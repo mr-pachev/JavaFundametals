4a885785990d044af5a41a090753fb294c1bde78
Ivan Pachev <mr.pachev@abv.bg>
2023-02-26 17:37:09 +0200

    Update HeroesOfCodeAndLogic7_03.java

--- JavaFundamentals/FinalExams/HeroesOfCodeAndLogic7_03.java
+++ JavaFundamentals/FinalExams/HeroesOfCodeAndLogic7_03.java
@@ -35,9 +35,9 @@ public class HeroesOfCodeAndLogic7_03 {
                     List<Integer> heroList = heroesMap.get(heroName);
                     int currentMP = heroList.get(1);
 
-                    if (currentMP - neededMP < 0){
+                    if (currentMP - neededMP < 0) {
                         System.out.printf("%s does not have enough MP to cast %s!%n", heroName, spellName);
-                    }else {
+                    } else {
                         System.out.printf("%s has successfully cast %s and now has %d MP!%n", heroName, spellName, currentMP - neededMP);
                         heroList.set(1, currentMP - neededMP);
                         heroesMap.put(heroName, heroList);
@@ -52,10 +52,10 @@ public class HeroesOfCodeAndLogic7_03 {
                     List<Integer> heroList = heroesMap.get(heroName);
                     int currentHP = heroList.get(0);
 
-                    if(currentHP - damage <= 0){
+                    if (currentHP - damage <= 0) {
                         heroesMap.remove(heroName);
                         System.out.printf("%s has been killed by %s!%n", heroName, attacker);
-                    }else {
+                    } else {
                         heroList.set(0, currentHP - damage);
                         heroesMap.put(heroName, heroList);
                         System.out.printf("%s was hit for %d HP by %s and now has %d HP left!%n", heroName, damage, attacker, currentHP - damage);
@@ -70,10 +70,10 @@ public class HeroesOfCodeAndLogic7_03 {
                     int currentMP = heroList.get(1);
                     int useMP = amountMP;
 
-                    if (currentMP + amountMP > 200){
+                    if (currentMP + amountMP > 200) {
                         heroList.set(1, 200);
                         useMP = 200 - currentMP;
-                    }else {
+                    } else {
                         currentMP += amountMP;
                         heroList.set(1, currentMP);
                     }
@@ -90,19 +90,31 @@ public class HeroesOfCodeAndLogic7_03 {
 
                     int usedHP = amountHP;
 
-                    if (currentHP + amountHP > 100){
+                    if (currentHP + amountHP > 100) {
                         heroList.set(0, 100);
-                        usedHP = 100 - amountHP;
-                    }else {
+                        usedHP = 100 - currentHP;
+                    } else {
                         currentHP += amountHP;
                         heroList.set(0, currentHP);
                     }
                     System.out.printf("%s healed for %d HP!%n", heroName, usedHP);
+                    heroesMap.put(heroName, heroList);
                     break;
                 }
             }
 
             input = scanner.nextLine();
+        }
+        for (Map.Entry<String, List<Integer>> entry : heroesMap.entrySet()) {
+            System.out.println(entry.getKey());
+
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                if (i == 0) {
+                    System.out.printf(" HP : %d%n", entry.getValue().get(0));
+                } else if (i == 1) {
+                    System.out.printf(" HM : %d%n", entry.getValue().get(1));
+                }
+            }
         }
     }
 }
