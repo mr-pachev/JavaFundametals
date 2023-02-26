18b2f3de9bb7ba6529db2d85f6bb58c68fd46ee4
Ivan Pachev <mr.pachev@abv.bg>
2023-02-26 17:23:57 +0200

    Create HeroesOfCodeAndLogic7_03.java

---
+++ JavaFundamentals/FinalExams/HeroesOfCodeAndLogic7_03.java
@@ -0,0 +1,108 @@
+package FinalExams;
+
+import java.util.*;
+
+public class HeroesOfCodeAndLogic7_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Map<String, List<Integer>> heroesMap = new LinkedHashMap<>();
+
+        for (int hero = 0; hero < n; hero++) {
+            String heroData = scanner.nextLine();
+            String name = heroData.split(" ")[0];
+            int healPoints = Integer.parseInt(heroData.split(" ")[1]);
+            int manaPoints = Integer.parseInt(heroData.split(" ")[2]);
+            List<Integer> heroDataList = new ArrayList<>();
+
+            heroDataList.add(healPoints);
+            heroDataList.add(manaPoints);
+            heroesMap.put(name, heroDataList);
+        }
+
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+            String command = input.split("\\s-\\s")[0];
+
+            switch (command) {
+                case "CastSpell": {
+                    String heroName = input.split("\\s-\\s")[1];
+                    int neededMP = Integer.parseInt(input.split("\\s-\\s")[2]);
+                    String spellName = input.split("\\s-\\s")[3];
+
+                    List<Integer> heroList = heroesMap.get(heroName);
+                    int currentMP = heroList.get(1);
+
+                    if (currentMP - neededMP < 0){
+                        System.out.printf("%s does not have enough MP to cast %s!%n", heroName, spellName);
+                    }else {
+                        System.out.printf("%s has successfully cast %s and now has %d MP!%n", heroName, spellName, currentMP - neededMP);
+                        heroList.set(1, currentMP - neededMP);
+                        heroesMap.put(heroName, heroList);
+                    }
+                    break;
+                }
+                case "TakeDamage": {
+                    String heroName = input.split("\\s-\\s")[1];
+                    int damage = Integer.parseInt(input.split("\\s-\\s")[2]);
+                    String attacker = input.split("\\s-\\s")[3];
+
+                    List<Integer> heroList = heroesMap.get(heroName);
+                    int currentHP = heroList.get(0);
+
+                    if(currentHP - damage <= 0){
+                        heroesMap.remove(heroName);
+                        System.out.printf("%s has been killed by %s!%n", heroName, attacker);
+                    }else {
+                        heroList.set(0, currentHP - damage);
+                        heroesMap.put(heroName, heroList);
+                        System.out.printf("%s was hit for %d HP by %s and now has %d HP left!%n", heroName, damage, attacker, currentHP - damage);
+                    }
+                    break;
+                }
+                case "Recharge": {
+                    String heroName = input.split("\\s-\\s")[1];
+                    int amountMP = Integer.parseInt(input.split("\\s-\\s")[2]);
+
+                    List<Integer> heroList = heroesMap.get(heroName);
+                    int currentMP = heroList.get(1);
+                    int useMP = amountMP;
+
+                    if (currentMP + amountMP > 200){
+                        heroList.set(1, 200);
+                        useMP = 200 - currentMP;
+                    }else {
+                        currentMP += amountMP;
+                        heroList.set(1, currentMP);
+                    }
+                    System.out.printf("%s recharged for %d MP!%n", heroName, useMP);
+                    heroesMap.put(heroName, heroList);
+                    break;
+                }
+                case "Heal": {
+                    String heroName = input.split("\\s-\\s")[1];
+                    int amountHP = Integer.parseInt(input.split("\\s-\\s")[2]);
+
+                    List<Integer> heroList = heroesMap.get(heroName);
+                    int currentHP = heroList.get(0);
+
+                    int usedHP = amountHP;
+
+                    if (currentHP + amountHP > 100){
+                        heroList.set(0, 100);
+                        usedHP = 100 - amountHP;
+                    }else {
+                        currentHP += amountHP;
+                        heroList.set(0, currentHP);
+                    }
+                    System.out.printf("%s healed for %d HP!%n", heroName, usedHP);
+                    break;
+                }
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
