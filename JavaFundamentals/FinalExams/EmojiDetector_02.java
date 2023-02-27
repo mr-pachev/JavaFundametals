d08ace4c149c3ce637df040064cd918c1d74b524
Ivan Pachev <mr.pachev@abv.bg>
2023-02-27 11:08:26 +0200

    Create EmojiDetector_02.java

---
+++ JavaFundamentals/FinalExams/EmojiDetector_02.java
@@ -0,0 +1,55 @@
+package FinalExams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmojiDetector_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern digitPattern = Pattern.compile("[0-9]");
+        Matcher digitMatcher = digitPattern.matcher(text);
+        int threshold = 1;
+
+        while (digitMatcher.find()) {
+            int currentDigit = Integer.parseInt(digitMatcher.group());
+            threshold *= currentDigit;
+        }
+
+        Pattern pattern = Pattern.compile("([\\::|\\**]{2})(?<emoji>[A-Z][a-z]{2,})\\1");
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> emojiList = new ArrayList<>();
+        List<String> onlyEmojiList = new ArrayList<>();
+
+        while (matcher.find()) {
+            String match = matcher.group();
+            String emojiClean = matcher.group("emoji");
+            onlyEmojiList.add(emojiClean);
+            emojiList.add(match);
+        }
+
+        for (int i = 0; i < onlyEmojiList.size(); i++) {
+            char[] word = onlyEmojiList.get(i).toCharArray();
+
+            if (charCount(word) < threshold) {
+                emojiList.remove(i);
+            }
+        }
+
+    }
+
+    public static Integer charCount(char[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count += arr[i];
+        }
+        return count;
+    }
+}
+
