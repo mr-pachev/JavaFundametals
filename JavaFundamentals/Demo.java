9b77579421077f49d5f9d5d783367ceb09766a19
Ivan Pachev <mr.pachev@abv.bg>
2023-03-19 14:19:45 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -1,52 +1,53 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int health = 100;
-        int bitcoins = 0;
-        String[] roomArr = scanner.nextLine().split("\\|");
-        int roomCounter = 0;
-        int bestRoom = 0;
+        List<String> inputList = Arrays.stream(scanner.nextLine().split(", "))
+                .collect(Collectors.toList());
+        ;
 
+        String input = scanner.nextLine();
 
-        for (int i = 0; i < roomArr.length; i++) {
-            roomCounter++;
-            String command = roomArr[i].split(" ")[0];
-            int number = Integer.parseInt(roomArr[i].split(" ")[1]);
+        while (!input.equals("Craft!")) {
+            String command = input.split(" - ")[0];
+            String element = input.split(" - ")[1];
 
-            if (command.equals("potion")) {
-                if (health + number > 100) {
-                    System.out.printf("You healed for %d hp.%n", 100 - health);
-                    System.out.println("Current health: 100 hp.");
-                    health = 100;
-                } else {
-                    health += number;
-                    System.out.printf("You healed for %d hp.%n", number);
-                    System.out.printf("Current health: %d hp.%n", health);
-                }
-
-            } else if (command.equals("chest")) {
-                bitcoins += number;
-                System.out.printf("You found %d bitcoins.%n", number);
-
-            } else {
-                health -= number;
-                if (health > 0) {
-                    System.out.printf("You slayed %s.%n", command);
-                } else {
-                    System.out.printf("You died! Killed by %s.%n", command);
-                    System.out.printf("Best room: %d%n", roomCounter);
+            switch (command) {
+                case "Collect":
+                    if (!inputList.contains(element)) {
+                        inputList.add(element);
+                    }
                     break;
-                }
+                case "Drop":
+                    for (int i = 0; i < inputList.size(); i++) {
+                        String current = inputList.get(i);
+
+                        if (element.equals(current)){
+                            inputList.remove(i);
+                            break;
+                        }
+                    }
+                    break;
+                case "Combine Items":
+                    String oldElement = element.split("\\:")[0];
+                    String newElement = element.split("\\:")[1];
+                    if (inputList.contains(oldElement)) {
+                        int startIndex = inputList.lastIndexOf(oldElement);
+                        inputList.add(startIndex, newElement);
+                    }
+                    break;
+                case "Renew":
+                    break;
             }
+
+
+            input = scanner.nextLine();
         }
-            if (health > 0){
-                System.out.println("You've made it!");
-                System.out.printf("Bitcoins: %d%n", bitcoins);
-                System.out.printf("Health: %d", health);
-            }
+
     }
 }
