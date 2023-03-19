22a5478fec9461e41a5eaf28a05b9f823fbdd9a6
Ivan Pachev <mr.pachev@abv.bg>
2023-03-19 12:02:08 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -9,22 +9,22 @@ public class Demo {
         int sumCourses = Integer.parseInt(scanner.nextLine());    //общ брой лекций
         int coursesBonus = Integer.parseInt(scanner.nextLine());  //допълнителен бонус
 
-        double[] studentMaxBonus = new double[sumStudents];
-        double[] studentMaxVisits = new double[sumStudents];
+        double maxBonus = Double.MIN_VALUE;
+        double visitsStudent = 0.0;
 
         for (int currentStudent = 0; currentStudent < sumStudents; currentStudent++) {
             int sumVisitsStudent = Integer.parseInt(scanner.nextLine()); //брой посещения на дадения студент
 
-            double allBonus = sumVisitsStudent * 1.0 /sumCourses;
-            allBonus *= (5 + coursesBonus);
-            allBonus = Math.round(allBonus);
-            studentMaxBonus[currentStudent] = allBonus;
-            studentMaxVisits[currentStudent] = sumVisitsStudent;
-        }
-        Arrays.sort(studentMaxBonus);
-        Arrays.sort(studentMaxVisits);
+            double bonus = sumVisitsStudent * 1.0 / sumCourses;
+            bonus = Math.round(bonus * (5 + coursesBonus));
 
-        System.out.printf("Max Bonus: %.0f.%n", studentMaxBonus[studentMaxBonus.length - 1]);
-        System.out.printf("The student has attended %.0f lectures.", studentMaxVisits[studentMaxVisits.length - 1]);
+            if (maxBonus <= bonus){
+                maxBonus = bonus;
+                visitsStudent = sumVisitsStudent;
+            }
+        }
+
+        System.out.printf("Max Bonus: %.0f.%n", maxBonus);
+        System.out.printf("The student has attended %.0f lectures.", visitsStudent);
     }
 }
