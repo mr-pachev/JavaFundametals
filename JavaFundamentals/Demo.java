3d263a4ad565a224d87b88e9dfd7f9bdcae07c64
Ivan Pachev <mr.pachev@abv.bg>
2023-03-19 20:45:46 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -23,29 +23,81 @@ public class Demo {
         int line1 = 0;
         int line2 = 0;
 
-        if (x1 >= 0 && x2 >= 0){
+        if (x1 >= 0 && x2 >= 0) {
             line1 = Math.abs(x1 + x2);
-        }else if (x1 < 0 && x2 < 0){
+        } else if (x1 < 0 && x2 < 0) {
             line1 = Math.abs(x1 - x2);
-        }else if (x1 < 0 && x2 > 0){
+        } else if (x1 < 0 && x2 > 0) {
             line1 = Math.abs(x1) + Math.abs(x2);
-        }else if (x1 > 0 && x2 < 0){
+        } else if (x1 > 0 && x2 < 0) {
             line1 = Math.abs(x1) + Math.abs(x2);
         }
 
-        if (x11 >= 0 && x21 >= 0){
+        if (x11 >= 0 && x21 >= 0) {
             line2 = Math.abs(x1 + x2);
-        }else if (x1 < 0 && x21 < 0){
+        } else if (x1 < 0 && x21 < 0) {
             line2 = Math.abs(x11 - x21);
-        }else if (x11 < 0 && x21 > 0){
+        } else if (x11 < 0 && x21 > 0) {
             line2 = Math.abs(x11) + Math.abs(x21);
-        }else if (x11 > 0 && x21 < 0){
+        } else if (x11 > 0 && x21 < 0) {
             line2 = Math.abs(x11) + Math.abs(x21);
         }
 
-        int[] pointsArr = { Math.abs(x1),  Math.abs(x2),  Math.abs(x11),  Math.abs(x21)};
+        if (line1 >= line2) {
+            int[] pointsArr = {Math.abs(x1), y1, Math.abs(x2), y2, Math.abs(x11), y11, Math.abs(x21), y21};
+            int[] realArr = {x1, y1, x2, y2, x11, y11, x21, y21};
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+
+            for (int i = 0; i < pointsArr.length / 2; i+=2) {
+
+                if (pointsArr[i] <= min) {
+                    min = pointsArr[i];
+                    index = i;
+                }
+            }
+
+            for (int i = 0; i < realArr.length / 2; i++) {
+                if (i == index) {
+                    System.out.printf("(%d, %d)", realArr[i], realArr[i + 1]);
+                    i++;
+                }
+            }
+            for (int i = 0; i < realArr.length / 2; i++) {
+                if (i != index) {
+                    System.out.printf("(%d, %d)", realArr[i], realArr[i + 1]);
+                    i++;
+                }
+            }
 
 
-        System.out.println();
+        } else {
+            int[] pointsArr = {Math.abs(x1), y1, Math.abs(x2), y2, Math.abs(x11), y11, Math.abs(x21), y21};
+            int[] realArr = {x1, y1, x2, y2, x11, y11, x21, y21};
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+
+            for (int i = 4; i < pointsArr.length; i+=2) {
+
+                if (pointsArr[i] <= min) {
+                    min = pointsArr[i];
+                    index = i;
+                }
+            }
+
+            for (int i = 4; i < realArr.length; i++) {
+                if (i == index) {
+                    System.out.printf("(%d, %d)", realArr[i], realArr[i + 1]);
+                    i++;
+                }
+            }
+            for (int i = 4; i < realArr.length; i++) {
+                if (i != index) {
+                    System.out.printf("(%d, %d)", realArr[i], realArr[i + 1]);
+                    i++;
+                }
+            }
+        }
+
     }
 }
