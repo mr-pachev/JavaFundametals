eeb64ab40c5f180f25df082e2967c893affdc3bd
Ivan Pachev <mr.pachev@abv.bg>
2023-03-19 11:26:33 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -1,20 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int sumStudents = Integer.parseInt(scanner.nextLine());
-        int sumCourses = Integer.parseInt(scanner.nextLine());
-        int coursesBonus = Integer.parseInt(scanner.nextLine());
+        int sumStudents = Integer.parseInt(scanner.nextLine());   //брой студенти
+        int sumCourses = Integer.parseInt(scanner.nextLine());    //общ брой лекций
+        int coursesBonus = Integer.parseInt(scanner.nextLine());  //допълнителен бонус
 
-        int[] currentStudentArr = new int[sumStudents];
+        double[] studentMaxBonus = new double[sumStudents];
+        double[] studentMaxVisits = new double[sumStudents];
 
         for (int currentStudent = 0; currentStudent < sumStudents; currentStudent++) {
-            int sumVisitsStudent = Integer.parseInt(scanner.nextLine());
+            int sumVisitsStudent = Integer.parseInt(scanner.nextLine()); //брой посещения на дадения студент
 
-            currentStudentArr[currentStudent] = sumVisitsStudent;
+            double allBonus = sumVisitsStudent * 1.0 /sumCourses;
+            allBonus *= (5 + coursesBonus);
+            allBonus = Math.round(allBonus);
+            studentMaxBonus[currentStudent] = allBonus;
+            studentMaxVisits[currentStudent] = sumVisitsStudent;
         }
-        System.out.println();
+        Arrays.sort(studentMaxBonus);
+        Arrays.sort(studentMaxVisits);
+
+        System.out.printf("Max Bonus: %.0f.%n", studentMaxBonus[studentMaxBonus.length - 1]);
+        System.out.printf("The student has attended %.0f lectures.", studentMaxVisits[studentMaxVisits.length - 1]);
     }
 }
