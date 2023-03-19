a0b0c0ab42f7f89c7b2ce29fa8296b8bd815350f
Ivan Pachev <mr.pachev@abv.bg>
2023-03-19 13:01:00 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -11,42 +11,42 @@ public class Demo {
         int roomCounter = 0;
         int bestRoom = 0;
 
-        while (health > 0){
+
+        for (int i = 0; i < roomArr.length; i++) {
             roomCounter++;
+            String command = roomArr[i].split(" ")[0];
+            int number = Integer.parseInt(roomArr[i].split(" ")[1]);
 
-            for (int i = 0; i < roomArr.length; i++) {
-                String command = roomArr[i].split(" ")[0];
-                int number = Integer.parseInt(roomArr[i].split(" ")[1]);
+            if (command.equals("potion")) {
+                if (health + number > 100) {
+                    System.out.printf("You healed for %d hp.%n", 100 - health);
+                    System.out.println("Current health: 100 hp.");
+                    health = 100;
+                } else {
+                    health += number;
+                    System.out.printf("You healed for %d hp.%n", number);
+                    System.out.printf("Current health: %d hp.%n", health);
+                }
 
-                if (command.equals("potion")){
-                    if (health + number > 100){
-                        System.out.printf("You healed for %d hp.%n", 100 - health);
-                        System.out.println("Current health: 100 hp.");
-                        health = 100;
-                    }else {
-                        health += number;
-                        System.out.printf("You healed for %d hp.%n", number);
-                        System.out.printf("Current health: %d hp.", health);
-                    }
+            } else if (command.equals("chest")) {
+                bitcoins += number;
+                System.out.printf("You found %d bitcoins.%n", number);
 
-                }else if (command.equals("chest")){
-                    bitcoins += number;
-                    System.out.printf("You found %d bitcoins.%n", number);
-                }else {
-                    health -= number;
-                    if(health > 0){
-                        System.out.printf("You slayed %s.%n", command);
-                    }else {
-                        System.out.printf("You died! Killed by %s.%n", command);
-                        System.out.printf("Best room: %d%n", roomCounter);
-                    }
+            } else {
+                health -= number;
+                if (health > 0) {
+                    System.out.printf("You slayed %s.%n", command);
+                } else {
+                    System.out.printf("You died! Killed by %s.%n", command);
+                    System.out.printf("Best room: %d%n", roomCounter);
+                    break;
                 }
             }
-
-
-
         }
-
-
+            if (health > 0){
+                System.out.println("You've made it!");
+                System.out.printf("Bitcoins: %d%n", bitcoins);
+                System.out.printf("Health: %d", health);
+            }
     }
 }
