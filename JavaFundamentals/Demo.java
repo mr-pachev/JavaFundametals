33c36f75a8251886320d623faf17647aaac1abed
Ivan Pachev <mr.pachev@abv.bg>
2023-03-21 17:35:58 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -9,62 +9,24 @@ public class Demo {
 
         int x1 = Integer.parseInt(scanner.nextLine());
         int y1 = Integer.parseInt(scanner.nextLine());
-
         int x2 = Integer.parseInt(scanner.nextLine());
         int y2 = Integer.parseInt(scanner.nextLine());
 
         int x11 = Integer.parseInt(scanner.nextLine());
         int y11 = Integer.parseInt(scanner.nextLine());
+        int x12 = Integer.parseInt(scanner.nextLine());
+        int y12 = Integer.parseInt(scanner.nextLine());
 
-        int x21 = Integer.parseInt(scanner.nextLine());
-        int y21 = Integer.parseInt(scanner.nextLine());
-
-        double length1 = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-        double length2 = Math.sqrt(((x21 - x11) * (x21 - x11)) + ((y21 - y11) * (y21 - y11)));
-
-        int index = 0;
-
-        if (length1 >= length2) {
-            int[] arr = {x1, x2};
-            int[] realAtt = {x1, y1, x2, y2};
-            index = findClosestNumber(arr);
-            for (int i = 0; i < realAtt.length; i++) {
-                if (realAtt[i] == index) {
-                    System.out.printf("(%d, %d)", realAtt[i], realAtt[i + 1]);
-                    break;
-                }
-            }
-            for (int i = 0; i < realAtt.length; i++) {
-                if (realAtt[i] != index) {
-                    System.out.printf("(%d, %d)", realAtt[i], realAtt[i + 1]);
-                    break;
-                }
-            }
-
-        } else {
-            int[] arr = {x11, x21};
-            int[] realAtt = {x11, y11, x21, y21};
-            index = findClosestNumber(arr);
-            for (int i = 0; i < realAtt.length; i++) {
-                if (realAtt[i] == index) {
-                    System.out.printf("(%d, %d)", realAtt[i], realAtt[i + 1]);
-                    break;
-                }
-            }
-            for (int i = 0; i < realAtt.length; i++) {
-                if (realAtt[i] != index) {
-                    System.out.printf("(%d, %d)", realAtt[i], realAtt[i + 1]);
-                    break;
-                }
-            }
-        }
-
-
-        System.out.println();
+        printSmallerLine(x1, y1, x2, y2, x11, y11, x12, y12);
     }
 
+    //метод определящ дължина на права
+    public static Double length (int x1, int y1, int x2, int y2){
+        return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    }
     //метод за намиране на най-близкото число до нула от подаден масив
-    public static Integer findClosestNumber(int[] nums) {
+    public static Integer findClosestNumber(int x1, int y1) {
+        int[] nums = {x1, y1};
         Arrays.sort(nums);
         int ans = 0;
         int val = nums[0];
@@ -79,5 +41,22 @@ public class Demo {
         if (ans == 0) return nums[nums.length - 1];
         if (Math.abs(val) < ans) return val;
         return ans;
+    }
+
+    //метод принтиране първо на нан-близката точка от права
+    public static void printCloserPoints(int x1, int y1, int x2, int y2){
+        if (findClosestNumber(x1, y1) <= findClosestNumber(x2, y2)){
+            System.out.printf("(%d, %d)(%d, %d)", x1, y1, x2, y2);
+        }else {
+            System.out.printf("(%d, %d)(%d, %d)", x2, y2, x1, y1);
+        }
+    }
+
+    public static void printSmallerLine (int x1, int y1, int x2, int y2, int x11, int y11, int x12, int y12){
+        if(length(x1, y1, x2, y2) >= length(x11, y11, x12, y12)){
+            printCloserPoints(x1, y1, x2, y2);
+        }else {
+            printCloserPoints(x11, y11, x12, y12);
+        }
     }
 }
