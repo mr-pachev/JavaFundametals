e004ad8d43ee87194cde3074e7973522d4fe379e
Ivan Pachev <mr.pachev@abv.bg>
2023-03-24 20:11:58 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -5,23 +5,33 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       int days = Integer.parseInt(scanner.nextLine());
-       int dailyPlunder = Integer.parseInt(scanner.nextLine());
-       double expectedPlunder = Double.parseDouble(scanner.nextLine());
+        int days = Integer.parseInt(scanner.nextLine());
+        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        double expectedPlunder = Double.parseDouble(scanner.nextLine());
 
-       double plunder = 0;
-       int daysCounter = 0;
+        double plunder = 0;
 
         for (int day = 1; day <= days; day++) {
-            daysCounter++;
+            plunder += dailyPlunder;
 
-            if (daysCounter == 3){
-                
-            }else if (daysCounter == 5){
-
+            if (day % 3 == 0) {
+                double bonusPlunder = 0;
+                bonusPlunder = 50 * 1.0 / 100 * dailyPlunder;
+                plunder += bonusPlunder;
             }
-
+            if (day % 5 == 0) {
+                double losePlinder = 0;
+                losePlinder = 30 * 1.0 / 100 * plunder;
+                plunder -= losePlinder;
+            }
         }
 
+        if (plunder >= expectedPlunder) {
+            System.out.printf("Ahoy! %.2f plunder gained.", plunder);
+        } else {
+            double percent = 0;
+            percent = plunder * 100 / expectedPlunder;
+            System.out.printf("Collected only %.2f%% of the plunder.", percent);
+        }
     }
 }
