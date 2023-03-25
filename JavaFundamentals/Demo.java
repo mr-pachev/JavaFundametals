02d3dbc92f13b79cf791875b6451f605d856452a
Ivan Pachev <mr.pachev@abv.bg>
2023-03-25 20:03:57 +0200

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -5,116 +5,62 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> pirateShip = Arrays.stream(scanner.nextLine().split("\\>"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-
-        List<Integer> warShip = Arrays.stream(scanner.nextLine().split("\\>"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-
-        int maxHealth = Integer.parseInt(scanner.nextLine());
+        String rawActivationKey = scanner.nextLine();
 
         String input = scanner.nextLine();
-        boolean isEnded = false;
 
-        while (!input.equals("Retire")) {
-
-            String command = input.split(" ")[0];
+        while (!input.equals("Generate")) {
+            String command = input.split(">>>")[0];
 
             switch (command) {
+                case "Contains":
+                    String subs = input.split(">>>")[1];
 
-                case "Fire":
-                    int indexFire = Integer.parseInt(input.split(" ")[1]);
-                    int currentSectionCondition = Integer.parseInt(input.split(" ")[2]);
-
-                    if (isExist(warShip, indexFire)) {
-                        int warshipSectionHealth = warShip.get(indexFire);
-                        int damage = warshipSectionHealth - currentSectionCondition;
-
-                        if (damage <= 0) {
-                            System.out.println("You won! The enemy ship has sunken.");
-                            isEnded = true;
-                            input = "Retire";
-                            continue;
-                        } else {
-                            warShip.set(indexFire, damage);
-                        }
+                    if (rawActivationKey.contains(subs)) {
+                        System.out.printf("%s contains %s%n", rawActivationKey, subs);
+                    } else {
+                        System.out.println("Substring not found!");
                     }
                     break;
 
-                case "Defend":
-                    int startIndex = Integer.parseInt(input.split(" ")[1]);
-                    int endIndex = Integer.parseInt(input.split(" ")[2]);
-                    int defend = Integer.parseInt(input.split(" ")[3]);
+                case "Flip":
+                    String typeLetter = input.split(">>>")[1];
+                    int startIndex = Integer.parseInt(input.split(">>>")[2]);
+                    int endIndex = Integer.parseInt(input.split(">>>")[3]);
 
-                    if (isExist(pirateShip, startIndex) && isExist(pirateShip, endIndex)) {
+                    String firstPart = rawActivationKey.substring(0, endIndex - 1);
+                    String secondPart = rawActivationKey.substring(endIndex);
 
-                        for (int i = startIndex; i <= endIndex; i++) {
-                            int pirateSectionHealth = pirateShip.get(i);
-                            int damageCurrentSection = pirateSectionHealth - defend;
+                    String flipString = rawActivationKey.substring(startIndex, endIndex);
+                    flipString = typeLetters(flipString, typeLetter);
 
-                            if (damageCurrentSection <= 0) {
-                                System.out.println("You lost! The pirate ship has sunken.");
-                                isEnded = true;
-                                input = "Retire";
-                                break;
-                            } else {
-                                pirateShip.set(i, damageCurrentSection);
-                            }
-                        }
-                    }
-
-                    break;
-                case "Repair":
-                    int indexRepair = Integer.parseInt(input.split(" ")[1]);
-                    int repairHealth = Integer.parseInt(input.split(" ")[2]);
-
-                    if (isExist(pirateShip, indexRepair)) {
-                        int repairCurrentSection = pirateShip.get(indexRepair);
-                        int repair = repairCurrentSection + repairHealth;
-
-                        if (repair >= maxHealth) {
-                            pirateShip.set(indexRepair, maxHealth);
-                        } else {
-                            pirateShip.set(indexRepair, repair);
-                        }
-                    }
+                    rawActivationKey = firstPart.concat(flipString).concat(secondPart);
+                    System.out.println(rawActivationKey);
                     break;
 
-                case "Status":
-                    double limit = 20 * 1.0 / 100 * maxHealth;
-                    int countSection = 0;
+                case "Slice":
+                    int firstIndex = Integer.parseInt(input.split(">>>")[1]);
+                    int lastIndex = Integer.parseInt(input.split(">>>")[2]);
 
-                    for (int i = 0; i < pirateShip.size(); i++) {
-                        int currentSectionStatus = pirateShip.get(i);
+                    String startPart = rawActivationKey.substring(0, firstIndex - 1);
+                    String endPart = rawActivationKey.substring(lastIndex);
 
-                        if (currentSectionStatus < limit) {
-                            countSection++;
-                        }
-                    }
-                    System.out.printf("%d sections need repair.%n", countSection);
+                    rawActivationKey = startPart.concat(endPart);
+                    System.out.println(rawActivationKey);
                     break;
             }
-
             input = scanner.nextLine();
         }
-        if (!isEnded) {
-            System.out.printf("Pirate ship status: %d%nWarship status: %d", sumSectionStatus(pirateShip), sumSectionStatus(warShip));
-        }
+        System.out.printf("Your activation key is: %s", rawActivationKey);
     }
+    public static String typeLetters (String key, String type){
+        String letters = "";
 
-    public static boolean isExist(List<Integer> list, int index) {
-        return (index >= 0 && index < list.size());
-    }
-
-    public static Integer sumSectionStatus (List<Integer> list){
-        int counterSum = 0;
-        for (int i = 0; i < list.size(); i++) {
-            counterSum += list.get(i);
+        if (type.equals("Upper")){
+            letters = key.toUpperCase();
+        }else if (type.equals("Lower")){
+            letters = key.toLowerCase();
         }
-        return counterSum;
+        return letters;
     }
 }
