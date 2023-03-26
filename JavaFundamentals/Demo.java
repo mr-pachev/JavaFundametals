6c966ebe584b134925e87be762f51a32e7b20aca
Ivan Pachev <mr.pachev@abv.bg>
2023-03-26 12:03:21 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -8,45 +8,25 @@ public class Demo {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
+        List<Integer> dataList = new ArrayList<>();
+        Map<String, List<Integer>> conquestMap = new LinkedHashMap<>();
 
-        List<String> emojisList = new ArrayList<>();
-        List<String> coolEmojiList = new ArrayList<>();
-        int cool = 1;
+        while(!input.equals("Sail")){
+            String city = input.split("\\|\\|")[0];
+            int people = Integer.parseInt(input.split("\\|\\|")[1]);
+            int golds = Integer.parseInt(input.split("\\|\\|")[2]);
 
-        Pattern patternDigit = Pattern.compile("(?<digit>[0-9])");
-        Matcher matcherDigit = patternDigit.matcher(input);
-
-        while (matcherDigit.find()) {
-            cool *= Integer.parseInt(matcherDigit.group("digit"));
-        }
-
-        Pattern pattern = Pattern.compile("(:{2}|[*]{2})(?<emoji>[A-Z][a-z]{2,})\\1");
-        Matcher matcher = pattern.matcher(input);
-
-        while (matcher.find()) {
-            String currentEmoji = matcher.group("emoji");
-            String currentMatch = matcher.group();
-            emojisList.add(currentEmoji);
-
-            if (charSum(currentEmoji) >= cool) {
-                coolEmojiList.add(currentMatch);
+            if (!conquestMap.containsKey(city)){
+                dataList.add(people);
+                dataList.add(golds);
+                conquestMap.put(city, dataList);
+            }else {
+                dataList = conquestMap.get(city);
+                dataList.set(0, people);
+                dataList.set(1, golds);
             }
-        }
 
-        System.out.printf("Cool threshold: %d%n", cool);
-        System.out.printf("%d emojis found in the text. The cool ones are:%n", emojisList.size());
-        for (String s : coolEmojiList) {
-            System.out.println(s);
+            input = scanner.nextLine();
         }
-    }
-
-    //метод за сумиране на символите от даден String
-    public static Integer charSum(String emoji) {
-        char[] emojiChar = emoji.toCharArray();
-        int sumChar = 0;
-        for (int i = 0; i < emojiChar.length; i++) {
-            sumChar += emojiChar[i];
-        }
-        return sumChar;
     }
 }
