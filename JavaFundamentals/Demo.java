3bbf064caabac0dfb9fb3b94285aa1c45e03b4ac
Ivan Pachev <mr.pachev@abv.bg>
2023-03-26 11:16:10 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -1,69 +1,46 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String rawActivationKey = scanner.nextLine();
-
         String input = scanner.nextLine();
 
-        while (!input.equals("Generate")) {
-            String command = input.split(">>>")[0];
+        List<String> emojisList = new ArrayList<>();
+        int cool = 1;
 
-            switch (command) {
-                case "Contains":
-                    String subs = input.split(">>>")[1];
+        Pattern patternDigit = Pattern.compile("(?<digit>[0-9])");
+        Matcher matcherDigit = patternDigit.matcher(input);
 
-                    if (rawActivationKey.contains(subs)) {
-                        System.out.printf("%s contains %s%n", rawActivationKey, subs);
-                    } else {
-                        System.out.println("Substring not found!");
-                    }
-                    break;
-
-                case "Flip":
-                    String typeLetter = input.split(">>>")[1];
-                    int startIndex = Integer.parseInt(input.split(">>>")[2]);
-                    int endIndex = Integer.parseInt(input.split(">>>")[3]);
-
-                    String firstPart = rawActivationKey.substring(0, startIndex);
-                    String secondPart = rawActivationKey.substring(endIndex);
-
-                    String flipString = rawActivationKey.substring(startIndex, endIndex);
-                    flipString = typeLetters(flipString, typeLetter);
-
-                    rawActivationKey = "";
-                    rawActivationKey = firstPart.concat(flipString).concat(secondPart);
-                    System.out.println(rawActivationKey);
-                    break;
-
-                case "Slice":
-                    int firstIndex = Integer.parseInt(input.split(">>>")[1]);
-                    int lastIndex = Integer.parseInt(input.split(">>>")[2]);
-
-                    String startPart = rawActivationKey.substring(0, firstIndex);
-                    String endPart = rawActivationKey.substring(lastIndex);
-
-                    rawActivationKey = "";
-                    rawActivationKey = startPart.concat(endPart);
-                    System.out.println(rawActivationKey);
-                    break;
-            }
-            input = scanner.nextLine();
+        while (matcherDigit.find()) {
+            cool *= Integer.parseInt(matcherDigit.group("digit"));
         }
-        System.out.printf("Your activation key is: %s", rawActivationKey);
+
+        Pattern pattern = Pattern.compile("([:|*]{2})(?<emoji>[A-Z][a-z]{2,})\\1");
+        Matcher matcher = pattern.matcher(input);
+        int countEmoji = 0;
+
+        while (matcher.find()) {
+            String cuurentEmoji = matcher.group("emoji");
+            emojisList.add(cuurentEmoji);
+
+            if (charSum(cuurentEmoji) >= cool) {
+                countEmoji++;
+            }
+        }
+
+        System.out.println();
     }
 
-    public static String typeLetters(String key, String type) {
-        String letters = "";
-
-        if (type.equals("Upper")) {
-            letters = key.toUpperCase();
-        } else if (type.equals("Lower")) {
-            letters = key.toLowerCase();
+    public static Integer charSum(String emoji) {
+        char[] emojiChar = emoji.toCharArray();
+        int sumChar = 0;
+        for (int i = 0; i < emojiChar.length; i++) {
+            sumChar += emojiChar[i];
         }
-        return letters;
+        return sumChar;
     }
 }
