39119ee5dda3b80dd81b747426ed6d9c427fa86a
Ivan Pachev <mr.pachev@abv.bg>
2023-03-27 21:32:54 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -9,15 +9,15 @@ public class Demo {
         String stops = scanner.nextLine();
         String input = scanner.nextLine();
 
-        while (!input.equals("Travel")){
+        while (!input.equals("Travel")) {
             String command = input.split("\\:")[0];
 
-            switch (command){
+            switch (command) {
                 case "Add Stop":
                     int indexAdd = Integer.parseInt(input.split("\\:")[1]);
                     String stopAdd = input.split("\\:")[2];
 
-                    if(isValid(stops, indexAdd)){
+                    if (isValid(stops, indexAdd)) {
                         String startPart = stops.substring(0, indexAdd);
                         String endPart = stops.substring(indexAdd);
                         stops = "";
@@ -30,7 +30,7 @@ public class Demo {
                     int startIndexRemove = Integer.parseInt(input.split("\\:")[1]);
                     int endIndexRemove = Integer.parseInt(input.split("\\:")[2]);
 
-                    if(isValid(stops,startIndexRemove) && isValid(stops, endIndexRemove)){
+                    if (isValid(stops, startIndexRemove) && isValid(stops, endIndexRemove)) {
                         String firstPart = stops.substring(0, startIndexRemove);
                         String secondPart = stops.substring(endIndexRemove);
                         stops = "";
@@ -40,16 +40,29 @@ public class Demo {
 
                     break;
                 case "Switch":
+                    String oldString = input.split("\\:")[1];
+                    String newString = input.split("\\:")[2];
+
+                    if (stops.contains(oldString)) {
+                        int startInt = stops.indexOf(oldString);
+                        int endInt = startInt + oldString.length();
+
+                        String first = stops.substring(0, startInt);
+                        String second = stops.substring(endInt);
+                        stops = "";
+                        stops = stops.concat(first).concat(newString).concat(second);
+                        System.out.println(stops);
+                    }
                     break;
             }
 
-
             input = scanner.nextLine();
         }
-
+        System.out.printf("Ready for world tour! Planned stops: %s", stops);
 
     }
-    public static boolean isValid (String stops, int index){
+
+    public static boolean isValid(String stops, int index) {
         return index >= 0 && index < stops.length();
     }
 }
