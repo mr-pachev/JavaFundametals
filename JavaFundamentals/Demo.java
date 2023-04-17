ad495e8f2c61ee555eb1919ab6b021364ccca154
Ivan Pachev <mr.pachev@abv.bg>
2023-04-17 10:51:13 +0300

    Create Demo.java

---
+++ JavaFundamentals/Demo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int allDay = Integer.parseInt(scanner.nextLine());
+        int targetPlunder = Integer.parseInt(scanner.nextLine());
+        int expectedPlunder = Integer.parseInt(scanner.nextLine());
+
+        int bonus = 0;
+        int loss = 0;
+
+        for (int day = 1; day <= allDay; day++) {
+
+
+
+
+
+        }
+    }
+}
