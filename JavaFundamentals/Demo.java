7299d2a1eaad28a7457043bc054bc7c43bfe6ad9
Ivan Pachev <mr.pachev@abv.bg>
2023-04-18 12:11:22 +0300

    Demo

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -7,48 +7,54 @@ public class Demo {
 
         int people = Integer.parseInt(scanner.nextLine());
         int[] liftArr = Arrays.stream(scanner.nextLine()
-                        .split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+                        .split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
 
         for (int i = 0; i < liftArr.length; i++) {
             int currentCabin = liftArr[i];
 
-            if (currentCabin < 4) {
-                int freeSpace = 4 - currentCabin;
-                if (freeSpace <= people) {
-                    liftArr[i] += freeSpace;
-                    people -= freeSpace;
-                }else {
-                    liftArr[i] = people;
-                    people = 0;
-                }
+            if (currentCabin < 4) {                     //проверка дали има сводни места в кабинката
+                int freeSpace = 4 - currentCabin;       //определяне на свободните места в кабинката
 
+                if (people > 0) {
+                    if (freeSpace <= people) {          //проверка дали свободните места са по-малко или равни на хората от опашката
+                        currentCabin += freeSpace;
+                        liftArr[i] = currentCabin;
+                        people -= freeSpace;
+                    } else {
+                        liftArr[i] += people;           //допълва се кабинката с хората от опашката
+                        people = 0;
+                    }
+                }
             }
         }
 
-        if (people == 0 || isFull(liftArr)){
-            if (people == 0 && !isFull(liftArr)){
+        if (people <= 0 || isFull(liftArr)) {
+            if (people == 0 && !isFull(liftArr)) {
                 System.out.println("The lift has empty spots!");
                 print(liftArr);
-            }else if (people != 0 && isFull(liftArr)){
+            } else if (people != 0 && isFull(liftArr)) {
                 System.out.printf("There isn't enough space! %d people in a queue!%n", people);
+                print(liftArr);
+            } else if (people <= 0 && isFull(liftArr)) {
                 print(liftArr);
             }
         }
+
     }
-    public static boolean isFull (int[] arr){
+
+    public static boolean isFull(int[] arr) {
         boolean isFull = true;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 4){
+            if (arr[i] < 4) {
                 isFull = false;
-                break;
             }
         }
         return isFull;
     }
 
-    public static void print (int[] arr){
+    public static void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d ", arr[i]);
         }
---
+++ JavaFundamentals/Demo1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int people = Integer.parseInt(scanner.nextLine());
+        int[] lift = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < lift.length; i++) {
+            if ((lift[i] < 4)) {                     //проверка дали има свободни места в кабинката
+                if (people >= 4 - lift[i]) {         //провера дали хората на опашката са равни или повече от свободните места в кабинката
+
+                    people -= 4 - lift[i];
+                    lift[i] = 4;
+                } else {
+                    lift[i] += people;
+                    people = 0;
+                }
+            }
+        }
+        boolean full = IntStream.range(0, lift.length).noneMatch(i -> lift[i] != 4);
+
+        if (people == 0 && !full) {
+            System.out.println("The lift has empty spots!");
+        } else if (people > 0 && full) {
+            System.out.printf("There isn't enough space! %d people in a queue!%n", people);
+        }
+        System.out.print(Arrays.toString(lift).replaceAll("[\\[\\]]", "").replaceAll(", ", " "));
+    }
+}
