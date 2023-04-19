f33c788a5f111b4ab3fc47d255094e9461465e6e
Ivan Pachev <mr.pachev@abv.bg>
2023-04-19 21:01:58 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -1,5 +1,5 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -12,37 +12,53 @@ public class Demo {
 
         String input = scanner.nextLine();
 
-        while (!input.equals("Yohoho!")){
+        while (!input.equals("Yohoho!")) {
             String command = input.split(" ")[0];
 
-            if (command.equals("Loot")){
+            if (command.equals("Loot")) {
                 List<String> lootList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
 
                 for (int i = 1; i < lootList.size(); i++) {
-                    if (!treasureList.contains(lootList.get(i))){
+                    if (!treasureList.contains(lootList.get(i))) {
                         treasureList.add(0, lootList.get(i));
                     }
                 }
 
-            }else if (command.equals("Drop")){
+            } else if (command.equals("Drop")) {
                 int removeIndex = Integer.parseInt(input.split(" ")[1]);
 
-                if (isValid(treasureList, removeIndex)){
+                if (isValid(treasureList, removeIndex)) {
                     String element = treasureList.get(removeIndex);
                     treasureList.remove(removeIndex);
                     treasureList.add(element);
                 }
-            }else if (command.equals("Steal")){
+            } else if (command.equals("Steal")) {
+                int stealCount = Integer.parseInt(input.split(" ")[1]);
 
+                if (stealCount >= treasureList.size()) { //проверка дали елементите за крадене са равни или повече от съкровището
+                    System.out.println(treasureList.toString().replaceAll("[\\[\\],]", "")
+                            .replaceAll(" ", ", "));
+                } else {
+                    int indexOf = treasureList.indexOf(treasureList.get(treasureList.size() - stealCount));
+                    List<String> stealList = new ArrayList<>();
+
+                    for (int i = indexOf; i < treasureList.size(); i++) {
+                        stealList.add(treasureList.get(i));
+                    }
+
+                    for (int i = 0; i < stealList.size(); i++) {
+                        treasureList.remove(stealList.get(i));
+                    }
+                    System.out.println(stealList.toString().replaceAll("[\\[\\],]", "")
+                            .replaceAll(" ", ", "));
+                }
             }
-
-
-
             input = scanner.nextLine();
         }
 
     }
-    public static boolean isValid (List<String> list, int index){
+
+    public static boolean isValid(List<String> list, int index) {
         return (index >= 0 && index < list.size());
     }
 }
