c3657c6b194a04008d5a85ae411b3681450186bb
Ivan Pachev <mr.pachev@abv.bg>
2023-04-19 20:22:04 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -2,63 +2,31 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] inputArr = Arrays.stream(scanner.nextLine()
-                        .split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        List<String> treasureList = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
 
-        double average = valueAverage(inputArr);
+        String input = scanner.nextLine();
 
-        int[] resultArr = new int[inputArr.length];
+        while (!input.equals("Yohoho!")){
+            String command = input.split(" ")[0];
 
-        for (int i = 0; i < inputArr.length; i++) {
-            if (inputArr[i] > average) {
-                resultArr[i] = inputArr[i];
+            if (command.equals("Loot")){
+
+            }else if (command.equals("Drop")){
+
+            }else if (command.equals("Steal")){
+
             }
+
+
+
+            input = scanner.nextLine();
         }
 
-        if (!isNegative(resultArr)) {
-            resultArr = Arrays.stream(resultArr)
-                    .boxed()
-                    .sorted(Collections.reverseOrder())
-                    .mapToInt(Integer::intValue)
-                    .toArray();
-        }
-
-        if (Arrays.stream(resultArr).sum() == 0) {
-            System.out.println("No");
-        } else {
-            for (int i = 0; i < 5; i++) {
-                if (resultArr[i] != 0) {
-                    System.out.printf("%d ", resultArr[i]);
-                }
-            }
-        }
-    }
-
-    //метод за намиране средната стойност на елементите от масив
-    public static Double valueAverage(int[] exampleArr) {
-        double result = 0;
-        for (int index = 0; index <= exampleArr.length - 1; index++) {
-            result += exampleArr[index];
-        }
-        result = result / exampleArr.length;
-        return result;
-    }
-
-    //проверка дали елементите са отрицателни числа
-    public static boolean isNegative(int[]arr){
-        boolean isNegative = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0){
-                isNegative = false;
-            }
-        }
-        return isNegative;
     }
 }
