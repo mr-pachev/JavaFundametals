0d0138f69b7f13edcfd54fb699e49d5e97be7d3e
Ivan Pachev <mr.pachev@abv.bg>
2023-04-19 20:42:41 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -16,9 +16,22 @@ public class Demo {
             String command = input.split(" ")[0];
 
             if (command.equals("Loot")){
+                List<String> lootList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+
+                for (int i = 1; i < lootList.size(); i++) {
+                    if (!treasureList.contains(lootList.get(i))){
+                        treasureList.add(0, lootList.get(i));
+                    }
+                }
 
             }else if (command.equals("Drop")){
+                int removeIndex = Integer.parseInt(input.split(" ")[1]);
 
+                if (isValid(treasureList, removeIndex)){
+                    String element = treasureList.get(removeIndex);
+                    treasureList.remove(removeIndex);
+                    treasureList.add(element);
+                }
             }else if (command.equals("Steal")){
 
             }
@@ -28,5 +41,8 @@ public class Demo {
             input = scanner.nextLine();
         }
 
+    }
+    public static boolean isValid (List<String> list, int index){
+        return (index >= 0 && index < list.size());
     }
 }
