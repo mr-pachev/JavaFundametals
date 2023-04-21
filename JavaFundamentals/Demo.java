0c9904d3b8bc98a9e90946f7aac8c990761d1adf
Ivan Pachev <mr.pachev@abv.bg>
2023-04-21 17:06:19 +0300

    Update Demo.java

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -8,79 +8,44 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> treasureList = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
-
+        String codedText = scanner.nextLine();
         String input = scanner.nextLine();
 
-        while (!input.equals("Yohoho!")) {
-            String command = input.split(" ")[0];
+        while (!input.equals("Decode")){
+            String command = input.split("\\|")[0];
 
-            if (command.equals("Loot")) {
-                List<String> lootList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+            switch (command){
 
-                for (int i = 1; i < lootList.size(); i++) {
-                    if (!treasureList.contains(lootList.get(i))) {
-                        treasureList.add(0, lootList.get(i));
-                    }
-                }
+                case "Move":
+                    int sumLetters = Integer.parseInt(input.split("\\|")[1]);
 
-            } else if (command.equals("Drop")) {
-                int removeIndex = Integer.parseInt(input.split(" ")[1]);
+                    String moveText = codedText.substring(0, sumLetters);
+                    String endTextMove = codedText.substring(sumLetters);
+                    codedText = "";
+                    codedText = codedText.concat(endTextMove).concat(moveText);
+                    break;
 
-                if (isValid(treasureList, removeIndex)) {
-                    String element = treasureList.get(removeIndex);
-                    treasureList.remove(removeIndex);
-                    treasureList.add(element);
-                }
-            } else if (command.equals("Steal")) {
-                int stealCount = Integer.parseInt(input.split(" ")[1]);
+                case "Insert":
+                    int indexInsert = Integer.parseInt(input.split("\\|")[1]);
+                    String newLetter = input.split("\\|")[2];
 
-                if (stealCount >= treasureList.size()) { //проверка дали елементите за крадене са равни или повече от съкровището
-                    System.out.println(treasureList.toString().replaceAll("[\\[\\],]", "")
-                            .replaceAll(" ", ", "));
-                    treasureList = new ArrayList<>();
-                } else {
-                    int indexOf = treasureList.indexOf(treasureList.get(treasureList.size() - stealCount));
-                    List<String> stealList = new ArrayList<>();
+                    String startTextInsert = codedText.substring(0, indexInsert);
+                    String endTextInsert = codedText.substring(indexInsert);
 
-                    for (int i = indexOf; i < treasureList.size(); i++) {
-                        stealList.add(treasureList.get(i));
-                    }
+                    codedText = "";
+                    codedText = codedText.concat(startTextInsert).concat(newLetter).concat(endTextInsert);
+                    break;
 
-                    for (int i = 0; i < stealList.size(); i++) {
-                        treasureList.remove(stealList.get(i));
-                    }
-                    System.out.println(stealList.toString().replaceAll("[\\[\\],]", "")
-                            .replaceAll(" ", ", "));
-                }
+                case "ChangeAll":
+                    String oldLetters = input.split("\\|")[1];
+                    String newLetters = input.split("\\|")[2];
+
+                    codedText = codedText.replaceAll(oldLetters, newLetters);
+                    break;
             }
             input = scanner.nextLine();
         }
 
-        if (treasureList.isEmpty()){
-            System.out.println("Failed treasure hunt.");
-        }else {
-            double avrSum = avr(treasureList);
-            System.out.printf("Average treasure gain: %.2f pirate credits.", avrSum);
-        }
-    }
-
-    //метод за проверка дали индекса е валиден
-    public static boolean isValid(List<String> list, int index) {
-        return (index >= 0 && index < list.size());
-    }
-
-    //метод за намиране средната стойност на съкровището
-    public static Double avr (List<String> list){
-        double averageSum = 0.0;
-        double allSum = 0.0;
-
-        for (int i = 0; i < list.size(); i++) {
-            String currentElement = list.get(i);
-
-            allSum += currentElement.length();
-
-        }
-        return averageSum = allSum / list.size();
+        System.out.printf("The decrypted message is: %s", codedText);
     }
 }
